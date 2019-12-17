00f9e0b940ce66924d2450a4f22b2ab3de46b61e
Adi Eyal <adi@openup.org.za>
2019-12-17 16:03:25 +0200

    Added a profile model

--- wazimap_ng/datasets/models.py
+++ wazimap_ng/datasets/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+class Profile(models.Model):
+    geography = models.CharField(max_length=50)
+    data = JSONField()
