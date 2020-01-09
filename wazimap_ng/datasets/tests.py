66bab6e40bfa35ad087be62fc43166890022b707
BarisSari <sariba@itu.edu.tr>
2020-01-10 00:55:00 +0300

    Apply black

--- wazimap_ng/datasets/tests.py
+++ wazimap_ng/datasets/tests.py
@@ -37,12 +37,7 @@ class DatasetGeneralTestCase(APITestCase):
 
     def test_indicator_list_is_readonly(self):
         url = reverse("indicator-list")
-        data = {
-            "groups": [],
-            "name": "test",
-            "label": "test-label",
-            "dataset": 1,
-        }
+        data = {"groups": [], "name": "test", "label": "test-label", "dataset": 1}
         response = self.client.post(url, data=data)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
@@ -55,11 +50,7 @@ class DatasetGeneralTestCase(APITestCase):
     def test_indicator_data_view_is_readonly(self):
         indicator_id = 2
         url = reverse("indicator-data-view", kwargs={"indicator_id": indicator_id})
-        data = {"data": {
-            "Language": "Unspecified",
-            "Count": 0,
-            "geography": "TEST123",
-        }}
+        data = {"data": {"Language": "Unspecified", "Count": 0, "geography": "TEST123"}}
         response = self.client.post(url, data=data, format="json")
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
@@ -92,31 +83,18 @@ class DatasetGeneralTestCase(APITestCase):
             "label": "Youth population by gender",
             "indicator": {
                 "id": 4,
-                "groups": [
-                    "Gender"
-                ],
+                "groups": ["Gender"],
                 "name": "Youth population by gender",
                 "label": "Youth population by gender",
-                "dataset": {
-                    "id": 1,
-                    "name": "Census 2011 - Language"
-                }
+                "dataset": {"id": 1, "name": "Census 2011 - Language"},
             },
             "universe": {
                 "id": 1,
-                "filters": {
-                    "Age__in": [
-                        "15 - 19",
-                        "20 - 24"
-                    ]
-                },
+                "filters": {"Age__in": ["15 - 19", "20 - 24"]},
                 "name": "Youth",
                 "label": "Youth (15 - 24)",
-                "dataset": {
-                    "id": 1,
-                    "name": "Census 2011 - Language"
-                }
-            }
+                "dataset": {"id": 1, "name": "Census 2011 - Language"},
+            },
         }
         response = self.client.post(url, data=data, format="json")
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
