3876e298be22142a9ad5c32b8c8058a63805e792
Adi Eyal <adi@openup.org.za>
2020-01-09 11:24:08 +0200

    Removed old user files

--- wazimap_ng/users/models.py
+++ wazimap_ng/users/models.py
@@ -16,7 +16,3 @@ class User(AbstractUser):
         return self.username
 
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
