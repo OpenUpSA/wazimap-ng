1f08d101388cd245070a68de82999a45673f4834
BarisSari <sariba@itu.edu.tr>
2020-01-10 00:54:39 +0300

    Initial commit

---
+++ wazimap_ng/datasets/tests.py
@@ -0,0 +1,128 @@
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+from django.test import TestCase
+
+
+class DatasetGeneralTestCase(APITestCase):
+    def test_dataset_list_is_readonly(self):
+        url = reverse("dataset")
+        data = {"name": "test"}
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.patch(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.delete(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_dataset_indicator_list_is_readonly(self):
+        dataset_id = 1
+        url = reverse("dataset-indicator-list", kwargs={"dataset_id": dataset_id})
+        data = {
+            "groups": [],
+            "name": "test",
+            "label": "test-label",
+            "dataset": dataset_id,
+        }
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.patch(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.delete(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_indicator_list_is_readonly(self):
+        url = reverse("indicator-list")
+        data = {
+            "groups": [],
+            "name": "test",
+            "label": "test-label",
+            "dataset": 1,
+        }
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.patch(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.delete(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_indicator_data_view_is_readonly(self):
+        indicator_id = 2
+        url = reverse("indicator-data-view", kwargs={"indicator_id": indicator_id})
+        data = {"data": {
+            "Language": "Unspecified",
+            "Count": 0,
+            "geography": "TEST123",
+        }}
+        response = self.client.post(url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.patch(url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.delete(url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_profile_list_is_readonly(self):
+        url = reverse("profile-list")
+        data = {"name": "test"}
+        response = self.client.post(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.patch(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.delete(url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_profile_detail_is_readonly(self):
+        pk = 1
+        url = reverse("profile-detail", kwargs={"pk": pk})
+        data = {
+            "subcategory": "Youth",
+            "category": "Youth Demographics",
+            "key_metric": False,
+            "name": "Youth population by gender",
+            "label": "Youth population by gender",
+            "indicator": {
+                "id": 4,
+                "groups": [
+                    "Gender"
+                ],
+                "name": "Youth population by gender",
+                "label": "Youth population by gender",
+                "dataset": {
+                    "id": 1,
+                    "name": "Census 2011 - Language"
+                }
+            },
+            "universe": {
+                "id": 1,
+                "filters": {
+                    "Age__in": [
+                        "15 - 19",
+                        "20 - 24"
+                    ]
+                },
+                "name": "Youth",
+                "label": "Youth (15 - 24)",
+                "dataset": {
+                    "id": 1,
+                    "name": "Census 2011 - Language"
+                }
+            }
+        }
+        response = self.client.post(url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.patch(url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.delete(url, data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
