4e5a7779b9e14244c510a904f03102c25ea09485
BarisSari <sariba@itu.edu.tr>
2020-01-12 21:44:37 +0300

    Complete IndicatorsGeographyTestCase tests

--- wazimap_ng/datasets/tests/test_globals.py
+++ wazimap_ng/datasets/tests/test_globals.py
@@ -245,7 +245,6 @@ class IndicatorsDetailTestCase(TestCase):
         self.assertEqual(results[1]["data"]["geography"], "second_code")
 
     def test_filtering_works(self):
-        # TODO: fails now
         url = reverse("indicator-data-view", kwargs={"indicator_id": self.indicator.pk})
         data = {"values": "Language:first_language"}
         response = self.client.get(url, data=data)
@@ -263,43 +262,39 @@ class IndicatorsDetailTestCase(TestCase):
 class IndicatorsGeographyTestCase(TestCase):
     def setUp(self):
         cache.clear()
+        get = lambda node_id: Geography.objects.get(pk=node_id)
         self.first_dataset = Dataset.objects.create(name="first")
-        self.geography = Geography.objects.create(
-            path="first_path",
-            depth=0,
-            name="first_geog",
-            code="first_code",
-            level="first_level",
+        self.root = Geography.add_root(
+            name="first_geog", code="first_code", level="first_level"
         )
         DatasetData.objects.create(
             dataset=self.first_dataset,
-            geography=self.geography,
-            data={
-                "Count": 100,
-                "Language": "first_language",
-                "another": 333,
-                "geography": "first_geog",
-            },
+            geography=self.root,
+            data={"Count": 1, "Language": "first_language"},
         )
-        self.geography_2 = Geography.objects.create(
-            path="second_path",
-            depth=0,
-            name="second_geog",
-            code="second_code",
-            level="second_level",
+        self.root_2 = get(self.root.pk).add_sibling(
+            name="second_geog", code="second_code", level="second_level"
+        )
+        self.child = get(self.root_2.pk).add_child(
+            name="child", code="child_geog", level="child_level"
         )
         DatasetData.objects.create(
             dataset=self.first_dataset,
-            geography=self.geography_2,
-            data={"Count": 1, "Language": "second_language", "another": 1},
+            geography=self.root_2,
+            data={"Count": 2, "Language": "second_language"},
         )
         DatasetData.objects.create(
             dataset=self.first_dataset,
-            geography=self.geography_2,
-            data={"Count": 2, "Language": "third_language", "another": 2},
+            geography=self.root_2,
+            data={"Count": 3, "Language": "third_language"},
+        )
+        DatasetData.objects.create(
+            dataset=self.first_dataset,
+            geography=self.child,
+            data={"Count": 4, "Language": "child_language"},
         )
         self.indicator = Indicator.objects.create(
-            groups=["Language"],
+            groups=["Language", "parent"],
             name="first_indicator",
             label="first_label",
             dataset=self.first_dataset,
@@ -310,7 +305,7 @@ class IndicatorsGeographyTestCase(TestCase):
             "indicator-data-view-geography",
             kwargs={
                 "indicator_id": self.indicator.pk,
-                "geography_code": self.geography_2.code,
+                "geography_code": self.root_2.code,
             },
         )
         response = self.client.get(url)
@@ -321,19 +316,19 @@ class IndicatorsGeographyTestCase(TestCase):
 
         results = response.data["results"]
         self.assertEqual(results[0]["data"]["Language"], "second_language")
-        self.assertEqual(results[0]["data"]["Count"], 1)
-        self.assertEqual(results[0]["data"]["geography"], "second_code")
-
-        self.assertEqual(results[0]["data"]["Language"], "third_language")
         self.assertEqual(results[0]["data"]["Count"], 2)
         self.assertEqual(results[0]["data"]["geography"], "second_code")
+
+        self.assertEqual(results[1]["data"]["Language"], "third_language")
+        self.assertEqual(results[1]["data"]["Count"], 3)
+        self.assertEqual(results[1]["data"]["geography"], "second_code")
 
     def test_filtering_works(self):
         url = reverse(
             "indicator-data-view-geography",
             kwargs={
                 "indicator_id": self.indicator.pk,
-                "geography_code": self.geography_2.code,
+                "geography_code": self.root_2.code,
             },
         )
         data = {"values": "Language:second_language"}
@@ -345,23 +340,28 @@ class IndicatorsGeographyTestCase(TestCase):
 
         results = response.data["results"]
         self.assertEqual(results[0]["data"]["Language"], "second_language")
-        self.assertEqual(results[0]["data"]["Count"], 1)
+        self.assertEqual(results[0]["data"]["Count"], 2)
         self.assertEqual(results[0]["data"]["geography"], "second_code")
 
     def test_parent_filtering_works(self):
-        # TODO: fails now
         url = reverse(
             "indicator-data-view-geography",
             kwargs={
                 "indicator_id": self.indicator.pk,
-                "geography_code": self.geography.code,
+                "geography_code": self.root_2.code,
             },
         )
         data = {"parent": True}
-        response = self.client.get(url, data=data)
+        response = self.client.get(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print("-" * 100)
-        print(response.data)
+
+        number_of_results = response.data["count"]
+        self.assertEqual(number_of_results, 1)
+
+        results = response.data["results"]
+        self.assertEqual(results[0]["data"]["Language"], "child_language")
+        self.assertEqual(results[0]["data"]["Count"], 4)
+        self.assertEqual(results[0]["data"]["geography"], "child_geog")
 
 
 class ProfileGeographyTestCase(TestCase):
