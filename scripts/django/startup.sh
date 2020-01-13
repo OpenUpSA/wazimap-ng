1fae8b7a8b872cc432794fd99d90a710755fd488
Adi Eyal <adi@openup.org.za>
2020-01-13 08:36:20 +0200

    Cloning repo now in Dockerfile

--- scripts/django/startup.sh
+++ scripts/django/startup.sh
@@ -1,6 +1,5 @@
 cd /app
-git clone https://github.com/OpenUpSA/wazimap-ng.git
-
 
 python3 manage.py collectstatic --noinput
+python3 manage.py migrate 
 exec /usr/local/bin/gunicorn wazimap_ng.wsgi -b 0.0.0.0:5000 --access-logfile /wazimap-ng.access.log --reload --error-logfile /wazimap-ng.error.log
