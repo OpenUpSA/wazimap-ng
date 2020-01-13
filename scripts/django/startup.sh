6a98bb7001cdc75d8af454447441d40eb1479fc0
Adi Eyal <adi@openup.org.za>
2020-01-13 08:08:41 +0200

    Changed port to 5000

--- scripts/django/startup.sh
+++ scripts/django/startup.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/OpenUpSA/wazimap-ng.git /app
-
 cd /app
+git clone https://github.com/OpenUpSA/wazimap-ng.git
+
 
 python3 manage.py collectstatic --noinput
-exec /usr/local/bin/gunicorn wazimap_ng.wsgi -b 0.0.0.0:8000 --access-logfile /wazimap-ng.access.log --reload --error-logfile /wazimap-ng.error.log
+exec /usr/local/bin/gunicorn wazimap_ng.wsgi -b 0.0.0.0:5000 --access-logfile /wazimap-ng.access.log --reload --error-logfile /wazimap-ng.error.log
