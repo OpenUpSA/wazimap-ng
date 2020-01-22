7d700e618781ad3915b13f95be1a430026f26c95
Adi Eyal <adi@openup.org.za>
2020-01-22 12:03:31 +0200

    Changed port to 8000

--- scripts/django/startup.sh
+++ scripts/django/startup.sh
@@ -2,4 +2,4 @@ cd /app
 
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate 
-exec /usr/local/bin/gunicorn wazimap_ng.wsgi -b 0.0.0.0:5000 --forwarded-allow-ips="*" --access-logfile /wazimap-ng.access.log --reload --error-logfile /wazimap-ng.error.log
+exec /usr/local/bin/gunicorn wazimap_ng.wsgi -b 0.0.0.0:8000 --forwarded-allow-ips="*" --access-logfile /wazimap-ng.access.log --reload --error-logfile /wazimap-ng.error.log
