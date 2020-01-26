4c0e37ed34f8339aa86eabd59c033f6c58a3671e
Adi Eyal <adi@openup.org.za>
2020-01-26 13:49:11 +0200

    Fixed geography treebeard model

--- wazimap_ng/datasets/models/geography.py
+++ wazimap_ng/datasets/models/geography.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.postgres.indexes import GinIndex
 
 from treebeard.mp_tree import MP_Node
-from treebeard.ns_tree import NS_NodeManager, NS_NodeQuerySet
+from treebeard.mp_tree import MP_NodeManager, MP_NodeQuerySet
 
 from django.contrib.postgres.search import TrigramSimilarity
 from wazimap_ng.extensions.index import GinTrgmIndex
 
 
-class GeographyQuerySet(NS_NodeQuerySet):
+class GeographyQuerySet(MP_NodeQuerySet):
     def search(self, text, similarity=0.3):
         return (
             self
@@ -17,7 +17,7 @@ class GeographyQuerySet(NS_NodeQuerySet):
                 .order_by("-similarity")
         )
 
-class GeographyManager(NS_NodeManager):
+class GeographyManager(MP_NodeManager):
     def get_queryset(self):
         return GeographyQuerySet(self.model, using=self._db)
 
