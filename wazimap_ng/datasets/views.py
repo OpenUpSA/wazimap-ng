7f30c710f1baa93791771a3935e8021d54c799e8
Gaurav Goyal <goyal1092@gmail.com>
2020-02-10 16:48:52 +0530

    Task-2 updating the profile view to use indicator data objects

--- wazimap_ng/datasets/views.py
+++ wazimap_ng/datasets/views.py
@@ -114,17 +114,23 @@ class ProfileDetail(generics.RetrieveAPIView):
 def get_children_data(children, indicator, subindicator):
     return {}
 
-def get_children_profile(profile_id, geography):
+def get_children_profile(profile_indicator_ids, geography):
     profile = {}
-    children_profiles = models.ProfileData.objects.filter(profile_id=profile_id, geography__in=geography.get_children()).select_related()
-    for child_profile in children_profiles:
-        if "indicators" in child_profile.data:
-            for indicator, subindicators in child_profile.data["indicators"].items():
-                indicator_data = profile.setdefault(indicator, {})
-                for subindicator in subindicators:
-                    key = subindicator["key"]
-                    subindicator_data = indicator_data.setdefault(key, {})
-                    subindicator_data[child_profile.geography.code] = subindicator["count"]
+    
+    children_profiles = models.IndicatorData.objects.filter(
+        indicator_id__in=profile_indicator_ids,
+        geography_id__in=geography.get_children().values_list("id", flat=True)
+    ).values("indicator__label","data", "geography__code", "indicator__groups")
+
+    for child in children_profiles:
+        groups = child.get("indicator__groups") or [None]
+        indicator_data = profile.setdefault(child.get("indicator__label"), {})
+        for subindicator in child.get("data"):
+            for group in groups:
+                key = subindicator.get(group, None)
+                subindicator_data = indicator_data.setdefault(key, {})
+                subindicator_data[child.get("geography__code")] = subindicator.get("Count")
+
     return profile
 
 @api_view()
@@ -132,68 +138,86 @@ def profile_geography_data(request, profile_id, geography_code):
     js = profile_geography_data_helper(profile_id, geography_code)
     return Response(js)
 
-@cache_decorator("profile_geography_data")
 def profile_geography_data_helper(profile_id, geography_code):
     profile = models.Profile.objects.get(pk=profile_id)
     geography = models.Geography.objects.get(code=geography_code)
+    profile_indicator_ids = profile.indicators.values_list("id", flat=True)
+
     geo_js = AncestorGeographySerializer().to_representation(geography)
     data_js = {}
     key_metrics = []
     highlights = {}
 
-    try:
-        profile_data = models.ProfileData.objects.get(profile_id=profile_id, geography=geography)
-        data = profile_data.data.get("indicators", {})
-        children_profiles = models.ProfileData.objects.filter(profile_id=profile_id, geography__in=geography.get_children())
-        children_profile = get_children_profile(profile_id, geography)
+    data = dict(models.IndicatorData.objects.filter(
+        indicator_id__in=profile_indicator_ids,
+        geography=geography
+    ).values_list("indicator__label","data"))
 
-        for pi in profile.profileindicator_set.order_by("subcategory__category__name", "subcategory__name").select_related():
-            indicator = pi.indicator
+    children_profile = get_children_profile(profile_indicator_ids, geography)
 
-            if pi.key_metric:
-                value = data.get(indicator.name, [{"count": "-"}])[0]
-                key_metrics.append({"label": pi.indicator.label, "value": value["count"]})
+    for pi in profile.profileindicator_set.order_by("subcategory__category__name", "subcategory__name").select_related():
+        indicator = pi.indicator
+        groups = pi.indicator.groups or [None]
+
+        if pi.key_metric:
+            value = data.get(indicator.label, [{"count": "-"}])[0]
+            key_metrics.append({"label": pi.label, "value": value["Count"]})
+        else:
+            subcategory = pi.subcategory
+            category = subcategory.category
+
+            category_js = data_js.setdefault(category.name, {})
+            category_js["description"] = category.description
+            subcats_js = category_js.setdefault("subcategories", {})
+            subcat_js = subcats_js.setdefault(subcategory.name, {})
+            subcat_js["description"] = subcategory.description
+            indicators_js  = subcat_js.setdefault("indicators", {})
+
+            indicator_data = data.get(pi.indicator.label, {})
+            indicators_js[pi.label] = {
+                "description": pi.description,
+                "subindicators": indicator_data
+            }
+
+            for subindicator in indicator_data:
+                if pi.indicator.label in children_profile:
+                    # TODO change name from children to child_geographies - need to change the UI as well
+                    for group in groups:
+                        key = subindicator[group] if group else None
+                        subindicator["children"] = children_profile[pi.indicator.label].get(key, 0)
+
+    highlights = {}
+
+    profile_highlights = profile.profilehighlight_set.all().values(
+        "name", "label", "indicator_id", "value", "indicator__groups"
+    )
+
+    indicators = dict(models.IndicatorData.objects.filter(
+        geography_id=geography.id, indicator_id__in=profile_highlights.values_list("indicator_id", flat=True)
+    ).values_list("indicator_id", "data"))
+
+    for highlight in profile_highlights:
+        indicator_id = highlight.get("indicator_id", None)
+        highlight_value = highlight.get("value")
+
+        if indicator_id in indicators:
+            data = indicators.get(indicator_id)
+            if highlight_value:
+                groups = highlight.get("indicator__groups", None)
+                count = 0
+                for val in data:
+                    for group in groups:
+                        if val[group] == highlight_value:
+                            count = count + val["Count"]
             else:
-                subcategory = pi.subcategory
-                category = subcategory.category
+                count = sum([val["Count"] for val in data])
+        else:
+            count = 0
 
-                category_js = data_js.setdefault(category.name, {})
-                category_js["description"] = category.description
-                subcats_js = category_js.setdefault("subcategories", {})
-                subcat_js = subcats_js.setdefault(subcategory.name, {})
-                subcat_js["description"] = subcategory.description
-                indicators_js  = subcat_js.setdefault("indicators", {})
-
-                indicator_data = data.get(pi.name, {})
-                indicators_js[pi.label] = {
-                    "description": pi.description,
-                    "subindicators": indicator_data
-                }
-
-                for subindicator in indicator_data:
-                    if pi.name in children_profile:
-                        # TODO change name from children to child_geographies - need to change the UI as well
-                        key = subindicator["key"]
-                        subindicator["children"] = children_profile[pi.name].get(key, 0)
-
-        for highlight in profile.profilehighlight_set.all():
-            highlight_data = profile_data.data.get("highlights", {})
-            indicator = pi.indicator;
-            if highlight.name in highlight_data:
-                value = highlight_data[highlight.name][0]["count"]
-
-                for v in highlight_data[highlight.name]:
-                    if v["key"] == highlight.value:
-                        value = v["count"]
-
-
-                highlights[highlight.name] = {
-                    "label": highlight.label,
-                    "count": value
-                }
-    except models.ProfileData.DoesNotExist:
-        print(f"ProfileData for {geography_code} does not exist")
-
+        highlights[highlight.get("name")] = {
+            "label": highlight.get("label"),
+            "count": count
+        }
 
     js = {
         "geography": geo_js,
