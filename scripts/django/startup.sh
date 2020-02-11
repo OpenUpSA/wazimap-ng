4b9c1a01a469f45d5b7764249d4eb87b6d72a8e9
Adi Eyal <adi@openup.org.za>
2020-02-11 23:52:46 +0200

    Increased gunicorn timeout to 300 in order to allow larger file uploads

--- scripts/django/startup.sh
+++ scripts/django/startup.sh
@@ -3,4 +3,4 @@ cd /app
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate 
 python3 manage.py qcluster& > /var/log/django-q.stdin.log 2> /var/log/django-q.stderr.log
-/usr/local/bin/gunicorn wazimap_ng.wsgi --workers=2 -b 0.0.0.0:8000 --forwarded-allow-ips="*" --access-logfile /wazimap-ng.access.log --reload --error-logfile /wazimap-ng.error.log
+/usr/local/bin/gunicorn wazimap_ng.wsgi --workers=2 -b 0.0.0.0:8000 --forwarded-allow-ips="*" --access-logfile /wazimap-ng.access.log --reload --error-logfile /wazimap-ng.error.log --timeout=300
