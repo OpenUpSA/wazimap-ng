79035e65bb9e17a417dfb51ba2b333ffb25cc9b7
Gaurav Goyal <goyal1092@gmail.com>
2020-02-21 14:01:14 +0530

    Fixed bug in checking for accurate data while uploading file

--- wazimap_ng/points/dataloader.py
+++ wazimap_ng/points/dataloader.py
@@ -12,17 +12,21 @@ def loaddata(name, iterable):
     category, created  = models.Category.objects.get_or_create(
         name="unspecified category", theme=theme
     )
-    keys = ["Name", "Longitude", "Latitude"]
-    datarows = []
 
     datarows = []
-
     for idx, row in enumerate(iterable):
 
-        for key in keys:
-            if key not in row:
-                print(f"Missing {key} - skipping it.")
-                continue
+        if "Longitude" not in row:
+            print(f"Missing Longitude - skipping it.")
+            continue
+
+        if "Longitude" not in row:
+            print(f"Missing Longitude - skipping it.")
+            continue
+
+        if "Longitude" not in row:
+            print(f"Missing Longitude - skipping it.")
+            continue
 
         location = row.pop("Name")
         latitude = row.pop("Latitude")
