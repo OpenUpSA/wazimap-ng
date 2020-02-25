fd936a7479e565e23f0fd8bc7799ff851cc10160
Gaurav Goyal <goyal1092@gmail.com>
2020-02-25 15:11:56 +0530

    Ability for admin to bulk assign points to categories form admin panel

--- wazimap_ng/points/admin.py
+++ wazimap_ng/points/admin.py
@@ -1,7 +1,7 @@
-#from django.contrib import admin
 from django.contrib.gis import admin
 from django_json_widget.widgets import JSONEditorWidget
 from django.contrib.postgres import fields
+from django.contrib import  messages
 
 from django_q.tasks import async_task
 from wazimap_ng.datasets import hooks
@@ -10,6 +10,16 @@ from wazimap_ng.datasets import hooks
 from . import models
 
 admin.site.register(models.Theme)
+
+def assign_to_category_action(category):
+    def assign_to_category(modeladmin, request, queryset):
+        queryset.update(category=category)
+        messages.info(request, "Locations assigned to category {0}".format(category.name))
+
+    assign_to_category.short_description = "Assign to {0}".format(category.name)
+    assign_to_category.__name__ = 'assign_to_category_{0}'.format(category.id)
+
+    return assign_to_category
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -24,6 +34,17 @@ class LocationAdmin(admin.OSMGeoAdmin):
 
     list_display = ("name", "category",)
     list_filter = ("category",)
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+
+        for category in models.Category.objects.all():
+            action = assign_to_category_action(category)
+            actions[action.__name__] = (
+                action, action.__name__, action.short_description
+            )
+
+        return actions
 
 @admin.register(models.CoordinateFile)
 class CoordinateFileAdmin(admin.ModelAdmin):
