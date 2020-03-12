12345a7e37676eb21ba0d11bbec2fd213f3e11ff
Gaurav Goyal <goyal1092@gmail.com>
2020-03-12 20:55:18 +0530

    Added geography levels details in locations api

--- wazimap_ng/points/serializers.py
+++ wazimap_ng/points/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from rest_framework import serializers
+from wazimap_ng.boundaries.models import GeographyBoundary
 
 from . import models
 
@@ -24,9 +25,24 @@ class ThemeSerializer(serializers.ModelSerializer):
 
 class LocationSerializer(GeoFeatureModelSerializer):
     category = CategorySerializer()
+    levels = serializers.SerializerMethodField('get_levels')
+
+    def get_levels(self, obj):
+        levels = {}
+        geographies = GeographyBoundary.objects.filter(
+            geom__contains=obj.coordinates
+        ).values("geography__level", "geography__id", "geography__name", "geography__code")
+
+        for geo in geographies:
+            levels[geo["geography__level"]] = {
+                "id": geo["geography__id"],
+                "name": geo["geography__name"],
+                "code": geo["geography__code"]
+            }
+        return levels
 
     class Meta:
         model = models.Location
         geo_field = "coordinates"
 
-        fields = ('id', 'category', 'data', "category")
+        fields = ('id', 'category', 'data', "category", "levels", )
