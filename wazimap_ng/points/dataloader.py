0b188074786efb6f78fdd000c8e33753c1dca5fb
Adi Eyal <adi@openup.org.za>
2020-03-19 21:35:43 +0200

    Catching errors when loading a coordinate file

--- wazimap_ng/points/dataloader.py
+++ wazimap_ng/points/dataloader.py
@@ -25,31 +25,35 @@ def loaddata(name, category, iterable):
     datarows = []
     for idx, row in enumerate(iterable):
 
-        if "longitude" not in row:
-            print(f"Missing Longitude - skipping it.")
+        try:
+            if "longitude" not in row:
+                print(f"Missing Longitude - skipping it.")
+                continue
+
+            if "latitude" not in row:
+                print(f"Missing Latitude - skipping it.")
+                continue
+
+            if "name" not in row:
+                print(f"Missing Name - skipping it.")
+                continue
+
+            location = row.pop("name")
+            coordinates = Point(row.pop("longitude"), row.pop("latitude"))
+
+            row["levels"] = get_levels(coordinates)
+            dd = models.Location(
+                name=location, category=category,
+                coordinates=coordinates, data=row
+            )
+
+            datarows.append(dd)
+
+            if len(datarows) >= 10000:
+                models.Location.objects.bulk_create(datarows, 1000)
+                datarows = []
+        except Exception:
+            # TODO Add log to help user identify the problem
             continue
-
-        if "latitude" not in row:
-            print(f"Missing Latitude - skipping it.")
-            continue
-
-        if "name" not in row:
-            print(f"Missing Name - skipping it.")
-            continue
-
-        location = row.pop("name")
-        coordinates = Point(row.pop("longitude"), row.pop("latitude"))
-
-        row["levels"] = get_levels(coordinates)
-        dd = models.Location(
-            name=location, category=category,
-            coordinates=coordinates, data=row
-        )
-
-        datarows.append(dd)
-
-        if len(datarows) >= 10000:
-            models.Location.objects.bulk_create(datarows, 1000)
-            datarows = []
 
     models.Location.objects.bulk_create(datarows, 1000)
