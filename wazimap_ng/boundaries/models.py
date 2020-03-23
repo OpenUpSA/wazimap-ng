3dc79480e1cc4897b7371bcf35e91b36eaf1719f
Adi Eyal <adi@openup.org.za>
2020-03-23 11:57:33 +0200

    Isolated code to handle duplicate geography codes
    This will need to be fixed in future

--- wazimap_ng/boundaries/models.py
+++ wazimap_ng/boundaries/models.py
@@ -6,6 +6,13 @@ from .fields import CachedMultiPolygonField
 from ..points.models import Location
 from django_q.tasks import async_task
 
+class GeographyBoundaryManager(models.Manager):
+    # Deal with a situation where there are multiple geographies with the same code
+    # TODO perhaps define a key to include level
+    def get_unique_boundary(self, code):
+        obj = GeographyBoundary.objects.filter(geography__code=code).first()
+        return obj
+
 class GeographyBoundary(models.Model):
     geography = models.ForeignKey(Geography, on_delete=models.PROTECT, null=True)
     code = models.CharField(max_length=10)
@@ -14,6 +21,7 @@ class GeographyBoundary(models.Model):
     area = models.FloatField()
     geom = models.MultiPolygonField(srid=4326, null=True)
     geom_cache = CachedMultiPolygonField(field_name="geom")
+    objects = GeographyBoundaryManager()
     
     class Meta:
         indexes = [models.Index(fields=["code"])]
