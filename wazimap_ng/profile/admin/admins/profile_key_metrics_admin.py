50ada52725bbd0c080eb19e980e5f4cc66c65d26
Adi Eyal <adi@openup.org.za>
2020-04-13 16:29:29 +0200

    Added profile filter to key metrics

--- wazimap_ng/profile/admin/admins/profile_key_metrics_admin.py
+++ wazimap_ng/profile/admin/admins/profile_key_metrics_admin.py
@@ -2,12 +2,17 @@ from django.contrib.gis import admin
 
 from ... import models
 from ..forms import ProfileKeyMetricsForm
+from wazimap_ng.admin_utils import customTitledFilter, description 
 
 @admin.register(models.ProfileKeyMetrics)
 class ProfileKeyMetricsAdmin(admin.ModelAdmin):
     fields = ('variable', 'subcategory', 'subindicator', 'denominator',)
     list_display = ('variable',)
     form = ProfileKeyMetricsForm
+
+    list_filter = (
+        ('subcategory__category__profile', customTitledFilter('Profile')),
+    )
 
 
     class Media:
