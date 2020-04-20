d308980262730bb3194fa6f3e1dcce11b24b6524
Adi Eyal <adi@openup.org.za>
2020-04-20 11:04:11 +0200

    Refactored cache code to prevent code duplication

--- wazimap_ng/cache.py
+++ wazimap_ng/cache.py
@@ -47,35 +47,36 @@ def last_modified_point_updated(request, category_id=None, theme_id=None):
     return last_modified(request, key)
 
 ########### Signals #################
-
-@receiver(post_save, sender=ProfileIndicator)
-def profile_indicator_updated(sender, instance, **kwargs):
-    profile_id = instance.profile.id
-    key = profile_key % profile_id
+def update_profile_cache(profile):
+    key = profile_key % profile.id
     cache.set(key, datetime.now())
 
-@receiver(post_save, sender=ProfileHighlight)
-def profile_highlight_updated(sender, instance, **kwargs):
-    profile_id = instance.profile.id
-    key = profile_key % profile_id
-    cache.set(key, datetime.now())
-
-@receiver(post_save, sender=Location)
-def point_updated_location(sender, instance, **kwargs):
-    point_updated_category(sender, instance.category, **kwargs)
-
-@receiver(post_save, sender=Category)
-def point_updated_category(sender, instance, **kwargs):
-    category_id = instance.id
-    theme_id = instance.theme.id
-    key1 = location_key % category_id
-    key2 = theme_key % theme_id
+def update_point_cache(category):
+    theme = category.theme
+    key1 = location_key % category.id
+    key2 = theme_key % theme.id
 
     logger.debug(f"Set cache key (category): {key1}")
     logger.debug(f"Set cache key (theme): {key2}")
 
     cache.set(key1, datetime.now())
     cache.set(key2, datetime.now())
+
+@receiver(post_save, sender=ProfileIndicator)
+def profile_indicator_updated(sender, instance, **kwargs):
+    update_profile_cache(instance.profile)
+
+@receiver(post_save, sender=ProfileHighlight)
+def profile_highlight_updated(sender, instance, **kwargs):
+    update_profile_cache(instance.profile)
+
+@receiver(post_save, sender=Location)
+def point_updated_location(sender, instance, **kwargs):
+    update_point_cache(instance)
+
+@receiver(post_save, sender=Category)
+def point_updated_category(sender, instance, **kwargs):
+    update_point_cache(instance)
 
 def cache_headers(func):
     return cache_control(max_age=0, public=True, must_revalidate=True)(func)
@@ -102,6 +103,3 @@ def cache_decorator(key, expiry=60*60*24*365):
             return obj
         return wrapper
     return _cache_decorator
-
-
-
