dbabd7f266b0128e11b207b5c765618717008618
Adi Eyal <adi@openup.org.za>
2020-04-21 11:08:24 +0200

    Fixed minor admin bug

--- wazimap_ng/profile/admin/admins/indicator_category_admin.py
+++ wazimap_ng/profile/admin/admins/indicator_category_admin.py
@@ -7,5 +7,5 @@ from ... import models
 @admin.register(models.IndicatorCategory)
 class IndicatorCategoryAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ("name", "profile", "order")
-    list_filter = ("profile")
+    list_filter = ("profile",)
 
