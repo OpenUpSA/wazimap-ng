91d99beb9463df8b5f4971753aecefc1a731a8e5
Gaurav Goyal <goyal1092@gmail.com>
2020-04-21 15:03:56 +0530

    Fixed bug after rebasing with staging

--- wazimap_ng/profile/admin/__init__.py
+++ wazimap_ng/profile/admin/__init__.py
@@ -5,8 +5,3 @@ from .admins import (
     LogoAdmin, ProfileIndicatorAdmin, ProfileKeyMetricsAdmin, ProfileHighlightAdmin,
     IndicatorCategoryAdmin, IndicatorSubcategoryAdmin, ProfileAdmin
 )
-
-admin.site.register(models.IndicatorCategory)
-admin.site.register(models.IndicatorSubcategory)
-
-
