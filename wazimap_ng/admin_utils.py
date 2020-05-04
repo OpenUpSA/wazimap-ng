7aa4de4d051a7da18b5baf79ebc23e6cf0293244
Adi Eyal <adi@openup.org.za>
2020-05-04 14:15:46 +0200

    Fixed group_packed not initialised error

--- wazimap_ng/admin_utils.py
+++ wazimap_ng/admin_utils.py
@@ -48,6 +48,8 @@ class GroupPermissionWidget(Widget):
 
     def get_groups(self, obj, user):
         all_groups = []
+        groups_packed = []
+        
         if obj.id:
             groups_with_permission = permissions.get_user_groups_with_permission_on_object(obj, user)
             groups_without_permission = permissions.get_user_groups_without_permission_on_object(obj, user)
