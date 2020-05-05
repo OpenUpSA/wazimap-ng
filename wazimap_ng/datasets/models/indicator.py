54a396855fd915f8bbcf811e638ae84cb83f592c
Adi Eyal <adi@openup.org.za>
2020-05-05 23:37:36 +0200

    Indicator now calculates its unique subindicators on first save

--- wazimap_ng/datasets/models/indicator.py
+++ wazimap_ng/datasets/models/indicator.py
@@ -14,6 +14,22 @@ class Indicator(models.Model):
     name = models.CharField(max_length=50)
     subindicators = JSONField(default=list, blank=True, null=True)
 
+    def get_unique_subindicators(self):
+        if len(self.groups) > 0:
+            # TODO this model should be refactored to only allow one group
+            group = self.groups[0]
+            subindicators = DatasetData.objects.filter(dataset=self.dataset).get_unique_subindicators(group)
+            return list(subindicators)
+
+        return []
+
+    def save(self, *args, **kwargs):
+        first_save = self.subindicators is None
+        if first_save:
+            self.subindicators = self.get_unique_subindicators()
+        super().save(*args, **kwargs)
+        
+
     def __str__(self):
         return f"{self.dataset.name} -> {self.name}"
 
