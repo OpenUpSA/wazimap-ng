5aca9cab65b5f57777da3f329e59856c2aff1df3
Adi Eyal <adi@openup.org.za>
2020-05-15 00:52:47 +0200

    Added additional configuration for CI

--- scripts/django/startup.sh
+++ scripts/django/startup.sh
@@ -1,11 +1,26 @@
 cd /app
 
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate 
+if [ "$RUN_COLLECTSTATIC" = "Yes" ]
+then
+    python3 manage.py collectstatic --noinput
+fi
+
+if [ "$RUN_MIGRATIONS" = "Yes" ]
+then
+    python3 manage.py migrate 
+fi
 
 if [ "$RUN_QCLUSTER" = "Yes" ]
 then
     python3 manage.py qcluster& > /var/log/django-q.stdin.log 2> /var/log/django-q.stderr.log
 fi
 
+if [ "$RUN_GUNICORN" = "Yes" ]
+then
 /usr/local/bin/gunicorn wazimap_ng.wsgi --workers=2 -b 0.0.0.0:8000 --forwarded-allow-ips="*" --access-logfile /wazimap-ng.access.log --reload --error-logfile /wazimap-ng.error.log --timeout=300
+fi
+
+if [ "$RUN_TESTS" = "Yes" ]
+then
+    /usr/local/bin/pytest /app/wazimap_ng/
+fi
