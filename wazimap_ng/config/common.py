9c117511300b1632db068c05635e4e8dca2ee14c
Adi Eyal <adi@openup.org.za>
2020-05-20 00:10:29 +0200

    Added additional cors settings

--- wazimap_ng/config/common.py
+++ wazimap_ng/config/common.py
@@ -261,6 +261,28 @@ class Common(Configuration):
     }
 
     CORS_ORIGIN_ALLOW_ALL = True
+    CORS_ALLOW_CREDENTIALS = True
+
+    CORS_ALLOW_METHODS = (
+        'DELETE',
+        'GET',
+        'OPTIONS',
+        'PATCH',
+        'POST',
+        'PUT',
+    )
+
+    CORS_ALLOW_HEADERS = (
+        'accept',
+        'accept-encoding',
+        'authorization',
+        'content-type',
+        'dnt',
+        'origin',
+        'user-agent',
+        'x-csrftoken',
+        'x-requested-with',
+    )
 
     Q_CLUSTER = {
        "orm": 'default',
