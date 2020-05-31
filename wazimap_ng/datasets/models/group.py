5f6f9b80b2c82a65290868886181c9e926b54d45
Adi Eyal <adi@openup.org.za>
2020-05-31 23:59:35 +0200

    Group is now using JSONField instead of ArrayField
    ArrayField is comma separated which is likely to be brittle

--- wazimap_ng/datasets/models/group.py
+++ wazimap_ng/datasets/models/group.py
@@ -1,15 +1,16 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import JSONField
 
 from .dataset import Dataset
 
 class Group(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False)
     dataset = models.ForeignKey(Dataset, null=True, on_delete=models.CASCADE)
-    subindicators = ArrayField(models.CharField(max_length=150), blank=True, default=list)
+    subindicators = JSONField(blank=True, default=list)
 
     def __str__(self):
         return str(self.name)
 
     class Meta:
         verbose_name = "SubindicatorsGroup"
+        ordering = ("name",)
