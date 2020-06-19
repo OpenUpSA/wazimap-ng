9031c39d68c84d1b4561dc0fcd0b40c456392d89
Gaurav Goyal <goyal1092@gmail.com>
2020-06-19 15:06:27 +0530

    Change geography hierarchy when profile is changed

--- wazimap_ng/general/static/js/geography_hierarchy.js
+++ wazimap_ng/general/static/js/geography_hierarchy.js
@@ -4,6 +4,10 @@ django.jQuery(document).ready(function($) {
         $("#id_geography_hierarchy").on('change', function() {
             loadHeirarchyHelpText();
         });
+
+        $("#id_profile").on('change', function() {
+            loadHeirarchyAccordingProfile();
+        });
     });
 
     function loadHeirarchyHelpText() {
@@ -29,6 +33,44 @@ django.jQuery(document).ready(function($) {
         } else {
             if ($help_div.length){
                 $help_div.empty();
+            }
+        }
+    }
+
+    function loadHeirarchyAccordingProfile() {
+        var profileID = $(document).find("#id_profile").val();
+        var geographyHierarchyID = $(document).find("#id_geography_hierarchy").val();
+        var $help_div = $(document).find(".field-geography_hierarchy .help");
+
+        if (profileID) {
+            var url = '/api/v1/profiles/' + profileID + "/";
+            $.ajax({
+                url: url,
+                success: function(data) {
+
+                    var hierarchyID = data.geography_hierarchy.id;
+                    
+                    if (hierarchyID != geographyHierarchyID){
+                        $(document).find("#id_geography_hierarchy").val(hierarchyID);
+                        if ($help_div.length){
+                            $help_div.empty();
+                        }else{
+                            $(document).find(".field-geography_hierarchy").append("<div class='help'></div>");
+                            $help_div = $(document).find(".field-geography_hierarchy .help");
+                        }
+                        let description = data.geography_hierarchy.description.length ? data.geography_hierarchy.description : "Description not Provided for this hierarchy."
+                        $help_div.append(data.name + " : " + description);
+                    }
+                    
+                }
+            })
+        } else {
+            if ($help_div.length){
+                $help_div.empty();
+            }
+
+            if (geographyHierarchyID){
+                $(document).find("#id_geography_hierarchy").val('');
             }
         }
     }
