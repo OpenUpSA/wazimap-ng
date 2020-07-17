97c9e0cdd986ee5465953ac7e0fabaf2573d24fa
Adi Eyal <adi@openup.org.za>
2020-07-17 12:56:15 +0200

    Switched back to an ORM queue from Redis

--- wazimap_ng/config/qcluster.py
+++ wazimap_ng/config/qcluster.py
@@ -1,17 +1,18 @@
 import os
+from wazimap_ng.utils import int_or_none, truthy
 
 
 class QCluster:
-    # Q_CLUSTER = {
-    #        "orm": 'default',
-    #        "retry": int(os.environ.get("Q_CLUSTER_RETRY", 100000)),
-    #        "workers": int(os.environ.get("Q_CLUSTER_WORKERS", 4)),
-    #        "recycle": int(os.environ.get("Q_CLUSTER_RECYCLE", 500)),
-    #        "timeout": int_or_none(os.environ.get("Q_CLUSTER_TIMEOUT", None)),
-    #        "ack_failures": truthy(os.environ.get("Q_CLUSTER_ACK_FAILURES", True)),
-    #        "sync": truthy(os.environ.get("Q_CLUSTER_SYNC", False)),
-    # }
-
     Q_CLUSTER = {
-        "redis": os.environ.get("REDIS_URL")
+           "orm": 'default',
+           "retry": int(os.environ.get("Q_CLUSTER_RETRY", 100000)),
+           "workers": int(os.environ.get("Q_CLUSTER_WORKERS", 4)),
+           "recycle": int(os.environ.get("Q_CLUSTER_RECYCLE", 500)),
+           "timeout": int_or_none(os.environ.get("Q_CLUSTER_TIMEOUT", None)),
+           "ack_failures": truthy(os.environ.get("Q_CLUSTER_ACK_FAILURES", True)),
+           "sync": truthy(os.environ.get("Q_CLUSTER_SYNC", False)),
     }
+
+    # Q_CLUSTER = {
+    #     "redis": os.environ.get("REDIS_URL")
+    # }
