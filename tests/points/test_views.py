b7b0b8e394eea366deae590991182dc966d11109
Adi Eyal <adi@openup.org.za>
2020-09-06 09:14:15 +0200

    Addition url reverse test for points

--- tests/points/test_views.py
+++ tests/points/test_views.py
@@ -3,21 +3,25 @@ from unittest.mock import Mock
 
 from django_mock_queries.query import MockSet, MockModel
 import pytest
-
 from django.urls import reverse
+from rest_framework.test import APIClient
 
 @pytest.fixture
 def api_client():
-   from rest_framework.test import APIClient
+   
    return APIClient()
 
-@pytest.mark.django_db
-def test_category_points_reverse_url(api_client):
-    url = reverse("category-points", kwargs={"profile_id": 1, "profile_category_id": 2})
-    assert url == "/api/v1/profile/1/points/category/2/points/"
+class TestCategoryView:
+    def test_category_points_reverse_url(self):
+        url = reverse("category-points", kwargs={"profile_id": 1, "profile_category_id": 2})
+        assert url == "/api/v1/profile/1/points/category/2/points/"
 
-@pytest.mark.django_db
-def test_category_points_geography_reverse_url(api_client):
-    url = reverse("category-points-geography", kwargs={"profile_id": 1, "profile_category_id": 2, "geography_code": "WC"})
-    assert url == "/api/v1/profile/1/points/category/2/geography/WC/points/"
+    def test_category_points_geography_reverse_url(self):
+        url = reverse("category-points-geography", kwargs={"profile_id": 1, "profile_category_id": 2, "geography_code": "WC"})
+        assert url == "/api/v1/profile/1/points/category/2/geography/WC/points/"
+
+class TestThemeView:
+    def test_reverse_url(self):
+        url = reverse("points-themes", kwargs={"profile_id": 1})
+        assert url == "/api/v1/profile/1/points/themes/"
 
