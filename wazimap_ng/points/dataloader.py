db1d4ec45c30bf14a8353e871ec34e32e678fa7f
Gaurav Goyal <goyal1092@gmail.com>
2020-09-18 12:10:03 +0530

    Show errors and error log file link for points upload

--- wazimap_ng/points/dataloader.py
+++ wazimap_ng/points/dataloader.py
@@ -12,14 +12,14 @@ def loaddata(category, iterable, row_number):
     for idx, row in enumerate(iterable):
 
         if "longitude" not in row:
-            logs.append([row_number+idx, "longitude", "Missing Header Longitude"])
+            logs.append([row_number+idx+1, "longitude", "Missing Header Longitude"])
 
         if "latitude" not in row:
-            logs.append([row_number+idx, "latitude", "Missing Header Latitude"])
+            logs.append([row_number+idx+1, "latitude", "Missing Header Latitude"])
             continue
 
         if "name" not in row:
-            logs.append([row_number+idx, "name", "Missing Header Name"])
+            logs.append([row_number+idx+1, "name", "Missing Header Name"])
             continue
 
         location = row.pop("name").strip()
@@ -34,28 +34,28 @@ def loaddata(category, iterable, row_number):
             longitude = float(longitude)
         except Exception as e:
             if not longitude:
-                logs.append([row_number+idx, "longitude", "Empty value for longitude"])
+                logs.append([row_number+idx+1, "longitude", "Empty value for longitude"])
             elif isinstance(longitude, str) and not longitude.isdigit():
-                logs.append([row_number+idx, "longitude", "Invalid value passed for longitude %s" % longitude])
+                logs.append([row_number+idx+1, "longitude", "Invalid value passed for longitude %s" % longitude])
             else:
-                logs.append([row_number+idx, "longitude", e])
+                logs.append([row_number+idx+1, "longitude", e])
             continue
 
         try:
             latitude = float(latitude)
         except Exception as e:
             if not latitude:
-                logs.append([row_number+idx, "latitude", "Empty value for latitude"])
+                logs.append([row_number+idx+1, "latitude", "Empty value for latitude"])
             elif isinstance(latitude, str) and not latitude.isdigit():
-                logs.append([row_number+idx, "latitude", "Invalid value passed for latitude %s" % latitude])
+                logs.append([row_number+idx+1, "latitude", "Invalid value passed for latitude %s" % latitude])
             else:
-                logs.append([row_number+idx, "latitude", e])
+                logs.append([row_number+idx+1, "latitude", e])
             continue
 
         try:
             coordinates = Point(longitude, latitude)
         except Exception as e:
-            logs.append([row_number+idx, "Coordinates", "Issue while creating coordinates %s " % e])
+            logs.append([row_number+idx+1, "Coordinates", "Issue while creating coordinates %s " % e])
             continue
 
         dd = models.Location(
@@ -68,7 +68,7 @@ def loaddata(category, iterable, row_number):
         if len(datarows) >= 10000:
             models.Location.objects.bulk_create(datarows, 1000)
             datarows = []
-       
+
 
     models.Location.objects.bulk_create(datarows, 1000)
     return logs
