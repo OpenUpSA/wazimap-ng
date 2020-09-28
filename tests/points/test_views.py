3b2212e7cf4969afc466fa812f6a3defcbadc231
milafrerichs <mila.frerichs@gmail.com>
2020-09-28 10:09:53 +0200

    chore: add another test for 404
    test the edge case when the result could not be retrieved from the
    database
    and rename the tests

--- tests/points/test_views.py
+++ tests/points/test_views.py
@@ -16,7 +16,7 @@ class TestCategoryView(APITestCase):
         self.get('category-points', profile_id=self.profile.pk, profile_category_id=self.profile_category.pk, extra={'format': 'json'})
         self.assert_http_200_ok()
 
-    def test_category_points_geography_reverse_url(self):
+    def test_category_points_geography_list(self):
         geography = GeographyFactory()
         GeographyBoundaryFactory(geography=geography)
         hierarchy = GeographyHierarchyFactory(root_geography=geography)
@@ -24,12 +24,17 @@ class TestCategoryView(APITestCase):
         self.get('category-points-geography', profile_id=profile.pk, profile_category_id=self.profile_category.pk, geography_code=geography.code, extra={'format': 'json'})
         self.assert_http_200_ok()
 
+    def test_category_points_geography_not_found(self):
+        geography = GeographyFactory()
+        self.get('category-points-geography', profile_id=self.profile.pk, profile_category_id=self.profile_category.pk, geography_code=geography.code, extra={'format': 'json'})
+        self.assert_http_404_not_found()
+
 class TestThemeView(APITestCase):
 
     def setUp(self):
         self.profile = ProfileFactory()
 
-    def test_reverse_url(self):
+    def test_points_themes_list(self):
         self.get("points-themes", profile_id=self.profile.pk)
         self.assert_http_200_ok()
 
