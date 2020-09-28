30f7b364479cd2811975167985552c96c44c01ba
Gaurav Goyal <goyal1092@gmail.com>
2020-09-28 08:15:57 +0530

    Fixed failing tests after view changes

--- tests/points/test_views.py
+++ tests/points/test_views.py
@@ -15,7 +15,7 @@ class TestCategoryView(APITestCase):
 
     def setUp(self):
         self.profile = ProfileFactory()
-        self.profile_category = ProfileCategoryFactory()
+        self.profile_category = ProfileCategoryFactory(profile=self.profile)
 
     def test_category_points_list(self):
         self.get('category-points', profile_id=self.profile.pk, profile_category_id=self.profile_category.pk, extra={'format': 'json'})
@@ -26,7 +26,8 @@ class TestCategoryView(APITestCase):
         GeographyBoundaryFactory(geography=geography)
         hierarchy = GeographyHierarchyFactory(root_geography=geography)
         profile = ProfileFactory(geography_hierarchy=hierarchy)
-        self.get('category-points-geography', profile_id=profile.pk, profile_category_id=self.profile_category.pk, geography_code=geography.code, extra={'format': 'json'})
+        profile_category = ProfileCategoryFactory(profile=profile)
+        self.get('category-points-geography', profile_id=profile.pk, profile_category_id=profile_category.pk, geography_code=geography.code, extra={'format': 'json'})
         self.assert_http_200_ok()
 
 class TestThemeView(APITestCase):
