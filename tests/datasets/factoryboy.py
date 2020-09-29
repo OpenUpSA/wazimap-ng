06f1e94074f172bdf50a2a7335d5d33cef984a50
milafrerichs <mila.frerichs@gmail.com>
2020-09-29 12:07:25 +0200

    feat: add view test for profile geography data
    to make sure that the indicator data is sorted properly
    this is a expected failing test to make sure the resulting group is
    ordered

--- tests/datasets/factoryboy.py
+++ tests/datasets/factoryboy.py
@@ -42,6 +42,14 @@ class IndicatorFactory(factory.django.DjangoModelFactory):
     universe = factory.SubFactory(UniverseFactory)
 
 
+class IndicatorDataFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.IndicatorData
+
+    indicator = factory.SubFactory(IndicatorFactory)
+    geography = factory.SubFactory(GeographyFactory)
+
+
 class GroupFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Group
