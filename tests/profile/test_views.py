37c082e553c78cd14425184f3e98fd4d126db880
milafrerichs <mila.frerichs@gmail.com>
2020-10-14 10:40:56 +0200

    chore: fix the usage of factories
    they got renamed but these needed to be updated

--- tests/profile/test_views.py
+++ tests/profile/test_views.py
@@ -4,8 +4,8 @@ from collections import OrderedDict
 from rest_framework.test import APIClient
 from django.urls import reverse
 
-from tests.profile import factoryboy as profile_factoryboy
-from tests.datasets import factoryboy as datasets_factoryboy
+from tests.profile.factories import ProfileFactory, IndicatorCategoryFactory, IndicatorSubcategoryFactory, ProfileIndicatorFactory
+from tests.datasets.factories import DatasetFactory, IndicatorFactory, IndicatorDataFactory, GroupFactory
 
 @pytest.fixture
 def api_client():
@@ -13,7 +13,7 @@ def api_client():
 
 @pytest.fixture
 def profile():
-    return profile_factoryboy.ProfileFactory()
+    return ProfileFactory()
 
 @pytest.fixture
 def indicator_data_items_data():
@@ -55,15 +55,15 @@ def expected_age_groups():
 class TestProfileGeographyData:
 
     def test_profile_geography_data_ordering_is_correct(self, api_client, profile, indicator_data_items_data, expected_age_groups):
-        dataset = datasets_factoryboy.DatasetFactory(geography_hierarchy=profile.geography_hierarchy, groups=["age group", "gender"])
-        indicator = datasets_factoryboy.IndicatorFactory(name="Age by Gender", dataset=dataset, groups=["gender"])
-        category = profile_factoryboy.IndicatorCategoryFactory(name="Category", profile=profile)
-        subcategory = profile_factoryboy.IndicatorSubcategoryFactory(category=category, name="Subcategory")
+        dataset = DatasetFactory(geography_hierarchy=profile.geography_hierarchy, groups=["age group", "gender"])
+        indicator = IndicatorFactory(name="Age by Gender", dataset=dataset, groups=["gender"])
+        category = IndicatorCategoryFactory(name="Category", profile=profile)
+        subcategory = IndicatorSubcategoryFactory(category=category, name="Subcategory")
 
-        indicator_data = datasets_factoryboy.IndicatorDataFactory(indicator=indicator, geography=profile.geography_hierarchy.root_geography, data=indicator_data_items_data)
-        profile_indicator = profile_factoryboy.ProfileIndicatorFactory(label="Indicator", profile=profile, indicator=indicator, subcategory=subcategory)
-        datasets_factoryboy.GroupFactory(name="age group", dataset=dataset, subindicators=["15-19", "20-24"]),
-        datasets_factoryboy.GroupFactory(name="gender", dataset=dataset, subindicators=["M", "F"]),
+        indicator_data = IndicatorDataFactory(indicator=indicator, geography=profile.geography_hierarchy.root_geography, data=indicator_data_items_data)
+        profile_indicator = ProfileIndicatorFactory(label="Indicator", profile=profile, indicator=indicator, subcategory=subcategory)
+        GroupFactory(name="age group", dataset=dataset, subindicators=["15-19", "20-24"]),
+        GroupFactory(name="gender", dataset=dataset, subindicators=["M", "F"]),
 
         url = reverse("profile-geography-data", kwargs={"profile_id": profile.pk, "geography_code": profile.geography_hierarchy.root_geography.code})
         response = api_client.get(url, format='json')
@@ -72,15 +72,15 @@ class TestProfileGeographyData:
         assert age_group == expected_age_groups
 
     def test_profile_geography_data_ordering_is_correct_order(self, api_client, profile, indicator_data_items_data):
-        dataset = datasets_factoryboy.DatasetFactory(geography_hierarchy=profile.geography_hierarchy, groups=["age group", "gender"])
-        indicator = datasets_factoryboy.IndicatorFactory(name="Age by Gender", dataset=dataset, groups=["gender"])
-        category = profile_factoryboy.IndicatorCategoryFactory(name="Category", profile=profile)
-        subcategory = profile_factoryboy.IndicatorSubcategoryFactory(category=category, name="Subcategory")
+        dataset = DatasetFactory(geography_hierarchy=profile.geography_hierarchy, groups=["age group", "gender"])
+        indicator = IndicatorFactory(name="Age by Gender", dataset=dataset, groups=["gender"])
+        category = IndicatorCategoryFactory(name="Category", profile=profile)
+        subcategory = IndicatorSubcategoryFactory(category=category, name="Subcategory")
 
-        indicator_data = datasets_factoryboy.IndicatorDataFactory(indicator=indicator, geography=profile.geography_hierarchy.root_geography, data=indicator_data_items_data)
-        profile_indicator = profile_factoryboy.ProfileIndicatorFactory(label="Indicator", profile=profile, indicator=indicator, subcategory=subcategory)
-        datasets_factoryboy.GroupFactory(name="age group", dataset=dataset, subindicators=["15-19", "20-24"]),
-        datasets_factoryboy.GroupFactory(name="gender", dataset=dataset, subindicators=["M", "F"]),
+        indicator_data = IndicatorDataFactory(indicator=indicator, geography=profile.geography_hierarchy.root_geography, data=indicator_data_items_data)
+        profile_indicator = ProfileIndicatorFactory(label="Indicator", profile=profile, indicator=indicator, subcategory=subcategory)
+        GroupFactory(name="age group", dataset=dataset, subindicators=["15-19", "20-24"]),
+        GroupFactory(name="gender", dataset=dataset, subindicators=["M", "F"]),
 
         url = reverse("profile-geography-data", kwargs={"profile_id": profile.pk, "geography_code": profile.geography_hierarchy.root_geography.code})
         response = api_client.get(url, format='json')
