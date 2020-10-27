bb293e8ccb11978456a6175a18c7377ea9b5ba90
Adi Eyal <adi@openup.org.za>
2020-10-27 15:58:59 +0200

    Refactored test code by extracting test data
    Makes it easier to re-use test code

--- tests/datasets/tasks/test_process_uploaded_file.py
+++ tests/datasets/tasks/test_process_uploaded_file.py
@@ -7,6 +7,17 @@ import pytest
 from wazimap_ng.datasets.tasks.process_uploaded_file import process_csv
 from tests.datasets.factories import DatasetFactory, GeographyFactory, GeographyHierarchyFactory
 
+def generate_file(data):
+    fp = tempfile.NamedTemporaryFile("w", delete=False)
+    writer = csv.writer(fp)
+    writer.writerow(["Geography", "field1", "field2", "count"])
+    writer.writerows(data)
+    filename = fp.name
+    fp.close()
+
+    return filename
+
+
 @pytest.fixture
 def geography_hierarchy():
     hierarchy = GeographyHierarchyFactory()
@@ -25,34 +36,25 @@ def dataset(geography_hierarchy):
     return DatasetFactory(geography_hierarchy=geography_hierarchy)
 
 @pytest.fixture
-def csv_file(geographies):
-    fp = tempfile.NamedTemporaryFile("w", delete=False)
-    writer = csv.writer(fp)
-    writer.writerow(["Geography", "field1", "field2", "count"])
-    writer.writerow([geographies[0].code, "F1_value_1", "F2_value_1", 111])
-    writer.writerow([geographies[1].code, "F1_value_2", "F2_value_2", 222])
-    filename = fp.name
-    fp.close()
-
-    return filename
-
+def good_data(geographies):
+    return [
+        (geographies[0].code, "F1_value_1", "F2_value_1", 111),
+        (geographies[1].code, "F1_value_2", "F2_value_2", 222),
+    ]
 
 @pytest.mark.django_db
 class TestUploadFile:
 
-    def test_process_csv(self, dataset, csv_file, geographies):
-        process_csv(dataset, csv_file)
+    def test_process_csv(self, dataset, good_data, geographies):
+        filename = generate_file(good_data)
+        process_csv(dataset, filename)
+
         datasetdata = dataset.datasetdata_set.all()
-        assert len(datasetdata) == 2
 
-        dd1 = datasetdata[0]
-        dd2 = datasetdata[1]
+        assert len(datasetdata) == len(good_data)
 
-        assert dd1.geography == geographies[0]
-        assert dd2.geography == geographies[1]
-
-
-        assert dd1.data["field1"] == "F1_value_1"
-        assert dd1.data["field2"] == "F2_value_1"
-        assert dd2.data["field1"] == "F1_value_2"
-        assert dd2.data["field2"] == "F2_value_2"
+        for dd, ed in zip(datasetdata, good_data):
+            assert dd.geography.code == ed[0]
+            assert dd.data["field1"] == ed[1]
+            assert dd.data["field2"] == ed[2]
+            assert dd.data["count"] == str(ed[3])
