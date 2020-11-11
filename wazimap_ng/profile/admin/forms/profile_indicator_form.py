d2f0627506be860798d9053b035748bffde76e27
Roman Melnyk <roman.melnyk20071992@gmail.com>
2020-11-12 01:02:58 +0200

    do not allow to save subcategory which not related to the selected profile

--- wazimap_ng/profile/admin/forms/profile_indicator_form.py
+++ wazimap_ng/profile/admin/forms/profile_indicator_form.py
@@ -1,14 +1,15 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from wazimap_ng.general.widgets import VariableFilterWidget
 from django_json_widget.widgets import JSONEditorWidget
 
-from ...models import IndicatorSubcategory, ProfileIndicator
+from ... import models
 
 
 class ProfileIndicatorAdminForm(forms.ModelForm):
     class Meta:
-        model = ProfileIndicator
+        model = models.ProfileIndicator
         fields = '__all__'
         widgets = {
             'indicator': VariableFilterWidget,
@@ -19,6 +20,28 @@ class ProfileIndicatorAdminForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # do not load any subcategories by default,
+        # do not show any subcategories by default,
         # it should be loaded based on selected profile via JS
-        self.fields['subcategory'].queryset = IndicatorSubcategory.objects.none()
+        self.fields['subcategory'].choices = [["", "-----------"]]
+
+        # filter subcategories by profile if initial data provided
+        if self.data.get('profile'):
+            subcategories = models.IndicatorSubcategory.objects.filter(
+                category__profile_id=self.data['profile']
+            )
+            self.fields['subcategory'].choices += [
+                [obj.id, obj.name] for obj in subcategories
+            ]
+
+
+
+    def clean_subcategory(self):
+        subcategory = self.cleaned_data['subcategory']
+        profile = self.cleaned_data['profile']
+
+        # do not allow to save subcategory which not related to the selected profile
+        if subcategory.category.profile != profile:
+            msg = f'This subcategory cannot be selected with {profile} profile.'
+            raise ValidationError(msg)
+
+        return subcategory
