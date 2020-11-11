4424d29694ac05121e69cda1756d347f25840395
Roman Melnyk <roman.melnyk20071992@gmail.com>
2020-11-12 01:01:57 +0200

    trigger "loadSubcategory" func only when it was manually changed

--- wazimap_ng/general/static/js/profile-indicator-admin.js
+++ wazimap_ng/general/static/js/profile-indicator-admin.js
@@ -1,11 +1,14 @@
 (function ($) {
     jQuery(document).ready(function ($) {
-        const $profileEl = $("#id_profile");
-        const $subcategoryEl = $("#id_subcategory");
+        const $profileEl = $(document).find("#id_profile");
+        const $subcategoryEl = $(document).find("#id_subcategory");
 
-        $profileEl.on('change', function (e) {
-            // load subcategories based on selected profile
-            loadSubcategory(e.target.value);
+        $(document).find("#id_profile").on('change', function (e) {
+            // trigger "loadSubcategory" func only when it was manually changed
+            if (e.originalEvent !== undefined) {
+                // load subcategories based on selected profile
+                loadSubcategory(e.target.value);
+            }
         });
 
         function clearSubcategories() {
@@ -14,11 +17,13 @@
         }
 
         function loadSubcategory(selectedProfile) {
+            console.log("selectedProfile", selectedProfile)
             if (selectedProfile) {
                 var url = `/api/v1/profiles/${selectedProfile}/subcategories/`;
                 $.ajax({
                     url: url,
                     success: function (data) {
+                        // clear it here to avoid "select" input flickering which appears during AJAX call
                         clearSubcategories();
                         $.each(data, function (key, value) {
                             $subcategoryEl.append($("<option></option>").attr("value", value.id).text(value.name));
