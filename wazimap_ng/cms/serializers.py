4e9b99a50e3b84268a5e00dee07a74c3feb25cb6
Gaurav Goyal <goyal1092@gmail.com>
2021-01-13 18:53:27 +0530

    Added pages and content in profile configs in api #189

---
+++ wazimap_ng/cms/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Content
+        fields = ("title", "text", "image", )
+
+class PageSerializer(serializers.ModelSerializer):
+    content_set = ContentSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = models.Page
+        fields = ("content_set", )
