a4fd0e58f8ff4d54a0f6fc60f52fb03d84c7c46e
Adi Eyal <adi@openup.org.za>
2021-02-08 21:33:20 +0200

    Minor refactor of unit tests to use fixtures

--- tests/datasets/models/test_groups.py
+++ tests/datasets/models/test_groups.py
@@ -3,16 +3,22 @@ from wazimap_ng.datasets.models import Indicator, Dataset, DatasetData, Group
 from unittest.mock import patch
 from unittest.mock import Mock
 
+import pytest
+
+@pytest.fixture
+def dataset():
+    return Dataset(name="test dataset")
+
+@pytest.fixture
+def group(dataset):
+    return Group(name="Hello", dataset=dataset)
+
 class TestGroup:
-    def test_create_group(self):
-        dataset = Dataset(name="test dataset")
-        g = Group(name="Hello", dataset=dataset)
-        assert len(g.subindicators) == 0
-        assert g.name == "Hello"
-        assert g.dataset == dataset
+    def test_create_group(self, dataset, group):
+        assert len(group.subindicators) == 0
+        assert group.name == "Hello"
+        assert group.dataset == dataset
 
 
-    def test_str(self):
-        d = Dataset(name="Test Dataset")
-        g = Group(name="Hello", dataset=d)
-        assert str(g) == f"Test Dataset|Hello"
+    def test_str(self, dataset, group):
+        assert str(group) == f"test dataset|Hello"
