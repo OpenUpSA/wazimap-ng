c209f4dd1cfb19f3ebd47ada55906e5d21eadcd9
Adi Eyal <adi@openup.org.za>
2021-02-10 10:55:38 +0200

    Replaced single quotes with double quotes

--- tests/datasets/tasks/indicator_data_extraction/test_indicator_data_extraction.py
+++ tests/datasets/tasks/indicator_data_extraction/test_indicator_data_extraction.py
@@ -48,40 +48,40 @@ class TestIndicatorDataExtract:
         indicator_data = indicator.indicatordata_set.first()
 
         assert indicator_data.data == {
-            'groups': {
-                'gender': {
-                    'male': [
-                        {'count': 11.0, 'age group': '15-19'},
-                        {'count': 21.0, 'age group': '20-24'},
-                        {'count': 31.0, 'age group': '25-29'}
+            "groups": {
+                "gender": {
+                    "male": [
+                        {"count": 11.0, "age group": "15-19"},
+                        {"count": 21.0, "age group": "20-24"},
+                        {"count": 31.0, "age group": "25-29"}
                     ],
-                    'female': [
-                        {'count': 10.0, 'age group': '15-19'},
-                        {'count': 20.0, 'age group': '20-24'},
-                        {'count': 30.0, 'age group': '25-29'}
+                    "female": [
+                        {"count": 10.0, "age group": "15-19"},
+                        {"count": 20.0, "age group": "20-24"},
+                        {"count": 30.0, "age group": "25-29"}
                     ]
                 }
             },
-            'subindicators': [
+            "subindicators": [
                 {
                     "group": "15-19",
                     "values": [
-                        {'count': 10.0, 'gender': 'male'},
-                        {'count': 20.0, 'gender': 'female'}
+                        {"count": 10.0, "gender": "male"},
+                        {"count": 20.0, "gender": "female"}
                     ]
                 },
                 {
                     "group": "20-24",
                     "values": [
-                        {'count': 11.0, 'gender': 'male'},
-                        {'count': 21.0, 'gender': 'female'}
+                        {"count": 11.0, "gender": "male"},
+                        {"count": 21.0, "gender": "female"}
                     ]
                 },
                 {
                     "group": "25-29",
                     "values": [
-                        {'count': 12.0, 'gender': 'male'},
-                        {'count': 22.0, 'gender': 'female'}
+                        {"count": 12.0, "gender": "male"},
+                        {"count": 22.0, "gender": "female"}
                     ]
                 }
             ]
