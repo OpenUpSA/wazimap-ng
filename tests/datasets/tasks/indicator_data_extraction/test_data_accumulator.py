aaf66cc540992661c924f36ab2e367c9cd984d9f
Adi Eyal <adi@openup.org.za>
2021-02-10 12:02:35 +0200

    Minor unit test refactor

--- tests/datasets/tasks/indicator_data_extraction/test_data_accumulator.py
+++ tests/datasets/tasks/indicator_data_extraction/test_data_accumulator.py
@@ -3,27 +3,26 @@ import pytest
 from wazimap_ng.datasets.tasks.indicator_data_extraction import DataAccumulator
 
 @pytest.fixture
-def geography_id():
-    return 4
+def data_accumulator():
+    geography_id = 4
+    return DataAccumulator(geography_id)
 
-def test_subindicators_no_groups(geography_id):
-    da = DataAccumulator(geography_id)
-
+def test_subindicators_no_groups(data_accumulator):
     data = [
         {"age group": "15-19", "count": 21.0},
         {"age group": "20-24", "count": 41.0},
         {"age group": "25-29", "count": 61.0}
     ]
 
-    da.add_subindicator(data)
+    data_accumulator.add_subindicator(data)
 
-    assert da.subindicators == [
+    assert data_accumulator.subindicators == [
         {"subindicator": "15-19", "count": 21.0},
         {"subindicator": "20-24", "count": 41.0},
         {"subindicator": "25-29", "count": 61.0},
     ]
 
-def test_subindicators_with_groups(geography_id):
+def test_subindicators_with_groups(data_accumulator):
     data = [
         {"age group": "15-19", "gender": "male", "count": 20.0},
         {"age group": "20-24", "gender": "male", "count": 40.0},
@@ -33,10 +32,10 @@ def test_subindicators_with_groups(geography_id):
         {"age group": "25-29", "gender": "female", "count": 61.0}
     ]
 
-    da = DataAccumulator(geography_id, primary_group="gender")
-    da.add_subindicator(data)
+    data_accumulator.primary_group = "gender"
+    data_accumulator.add_subindicator(data)
 
-    assert da.subindicators == [
+    assert data_accumulator.subindicators == [
         {
             "group": "male",
             "values": [
@@ -55,15 +54,15 @@ def test_subindicators_with_groups(geography_id):
         }
     ]
 
-def test_get_groups(geography_id):
+def test_get_groups(data_accumulator):
     data = [
         {"count": 23, "gender": "male", "age group": "15-19"},
         {"count": 46, "gender": "male", "age group": "20-24"},
         {"count": 23, "gender": "female", "age group": "15-19"},
     ]
 
-    da = DataAccumulator(geography_id, primary_group="gender")
-    groups = da.get_groups(data)
+    data_accumulator.primary_group = "gender"
+    groups = data_accumulator.get_groups(data)
 
     assert len(groups) == 2
     assert "gender" in groups
