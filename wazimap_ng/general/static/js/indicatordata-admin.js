c8681aa513aa3493d5e5ecb5997f1460d04ca2df
khadija <khadijamahanga@y7mail.com>
2021-02-24 10:03:47 +0300

    hide showing of option

--- wazimap_ng/general/static/js/indicatordata-admin.js
+++ wazimap_ng/general/static/js/indicatordata-admin.js
@@ -1,7 +1,60 @@
 (function($) {
     jQuery(document).ready(function($) {
-        var form = document.getElementById("indicatordata_form");
-        form.style.display = "none";
+
+        changeElDisplay("indicatordata_form", "none")
+        createSelectOption();
+
+        function selectOnChange(val) {
+            if (val == "2") {
+                changeElDisplay("indicatordata_form", "block");
+                changeElDisplay("add_method__div", "none");
+            } else {
+                changeElDisplay("indicatordata_form", "none");
+            }
+        }
+
+
+        function changeElDisplay(id, display) {
+            var el = document.getElementById(id);
+            el.style.display = display;
+        }
+        
+        function createSelectOption() {
+            var selDiv = document.createElement("div");
+            selDiv.setAttribute("class", "form-row aligned");
+            selDiv.setAttribute("id","add_method__div");
+    
+            var sel = document.createElement("select");
+            sel.setAttribute("id","add_method");
+            sel.onchange = function() { selectOnChange(this.value)};
+    
+            var selLabel = document.createElement("Label");
+            selLabel.setAttribute("for", "add_method");
+            selLabel.setAttribute("class", "required")
+            selLabel.innerHTML = "Choose add method";
+    
+            var optionValues = {
+                0:"----------------------", 
+                1:"Upload an indicator director",
+                2:"Create from dataset"
+            }
+            for (key in optionValues) {
+                var opt = document.createElement("option");
+                if (key == "0") {
+                    opt.value = ""
+                } else {
+                    opt.value = key;
+                }
+                opt.text = optionValues[key];
+                sel.add(opt)
+            }
+    
+            var content = document.getElementById("content-main");
+            selDiv.appendChild(selLabel);
+            selDiv.appendChild(sel);
+            content.appendChild(selDiv);
+
+        }
 
     });
 })(django.jQuery);
