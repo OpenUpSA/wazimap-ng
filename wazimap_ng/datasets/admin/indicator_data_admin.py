af8b8a8ab851172b764ce3827008e266a4c64000
khadija <khadijamahanga@y7mail.com>
2021-02-25 12:14:59 +0300

    add js + extend url

--- wazimap_ng/datasets/admin/indicator_data_admin.py
+++ wazimap_ng/datasets/admin/indicator_data_admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from django.contrib.postgres import fields
+from django.urls import path
 
 from django_json_widget.widgets import JSONEditorWidget
 
 from .base_admin_model import DatasetBaseAdminModel
 from .. import models
+from .views.indicator_director_view import IndicatorDirectorAdminView
 
 from wazimap_ng.general.admin import filters
 
 
 @admin.register(models.IndicatorData)
 class IndicatorDataAdmin(DatasetBaseAdminModel):
-    add_form_template = 'admin/indicatordata_change_list.html'
+    add_form_template = "admin/indicatordata_change_list.html"
 
     def indicator__name(self, obj):
         return obj.indicator.name
@@ -40,10 +42,19 @@ class IndicatorDataAdmin(DatasetBaseAdminModel):
 
     search_fields = ["geography__name"]
 
-    # class Media:
-    #     js = ("/static/js/indicatordata-admin.js",)
+    class Media:
+        js = ("/static/js/indicatordata-admin.js",)
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
             return ("geography", "indicator") + self.readonly_fields
         return self.readonly_fields
+    
+    def get_urls(self):
+        urls = super(IndicatorDataAdmin, self).get_urls()
+        my_urls = [
+            path("upload/", IndicatorDirectorAdminView.as_view(), name="upload_indicator_director"),
+        ]
+        return my_urls + urls
+
+
