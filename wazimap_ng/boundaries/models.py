1e601ed54048af6e630b4fdb0ce119e99b97b43c
milafrerichs <mila.frerichs@gmail.com>
2021-02-26 16:47:47 +0100

    fix: fix import issue and change style
    according to flake8 there were some style changes necessary

--- wazimap_ng/boundaries/models.py
+++ wazimap_ng/boundaries/models.py
@@ -1,11 +1,9 @@
-from django.contrib.gis.db import models
+from django.contrib.gis.db import models as gis_models
 from django.db import models
-from django_q.tasks import async_task
 
 from wazimap_ng.general.models import BaseModel
 
 from ..datasets.models import Geography
-from ..points.models import Location
 from .fields import CachedMultiPolygonField
 
 
@@ -13,14 +11,17 @@ class GeographyBoundaryManager(models.Manager):
     # Deal with a situation where there are multiple geographies with the same code
     # TODO perhaps define a key to include level
     def get_unique_boundary(self, geography):
-        #TODO might need to remove this now that code is not stored in the boundary
-        obj = GeographyBoundary.objects.filter(geography__code=geography.code, geography__version=geography.version).first()
+        # TODO might need to remove this now that code is not stored in the boundary
+        obj = GeographyBoundary.objects.filter(
+            geography__code=geography.code, geography__version=geography.version
+        ).first()
         return obj
+
 
 class GeographyBoundary(BaseModel):
     geography = models.OneToOneField(Geography, on_delete=models.PROTECT, null=False)
 
     area = models.FloatField()
-    geom = models.MultiPolygonField(srid=4326, null=True)
+    geom = gis_models.MultiPolygonField(srid=4326, null=True)
     geom_cache = CachedMultiPolygonField(field_name="geom")
     objects = GeographyBoundaryManager()
