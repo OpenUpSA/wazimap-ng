adbe4c7bd25f719d3504cff18c6c16d75e210c0b
khadija <khadijamahanga@y7mail.com>
2021-03-03 15:50:13 +0300

    use file field

--- wazimap_ng/datasets/admin/forms/indicator_director_admin_form.py
+++ wazimap_ng/datasets/admin/forms/indicator_director_admin_form.py
@@ -1,8 +1,8 @@
-from django.contrib.postgres.fields import JSONField
-
 from django import forms
 from ... import models
 
 class IndicatorDirectorForm(forms.Form):
     dataset = forms.ModelChoiceField(queryset=models.Dataset.objects.all(), required=True)
-    indicator_director = JSONField(required=True)
+    indicator_director = forms.FileField(required=True)
+
+        
