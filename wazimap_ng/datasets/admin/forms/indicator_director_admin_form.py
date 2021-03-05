6a9bc7921beef263873abc91c147432de72a40e1
Khadija Mahanga <khadijamahanga@y7mail.com>
2021-03-05 20:30:53 +0300

    Update wazimap_ng/datasets/admin/forms/indicator_director_admin_form.py
    Co-authored-by: _ Kilemensi <kilemensi@users.noreply.github.com>

--- wazimap_ng/datasets/admin/forms/indicator_director_admin_form.py
+++ wazimap_ng/datasets/admin/forms/indicator_director_admin_form.py
@@ -4,5 +4,3 @@ from ... import models
 class IndicatorDirectorForm(forms.Form):
     dataset = forms.ModelChoiceField(queryset=models.Dataset.objects.all(), required=True)
     indicator_director = forms.FileField(required=True)
-
-        
