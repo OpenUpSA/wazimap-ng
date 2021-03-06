ffc894a66d6a95d9c0d04acb83ab55627c1e8b62
Adi Eyal <adi@openup.org.za>
2021-03-06 16:14:55 +0200

    Updated get_indicator_data to accept multiple geographies

--- tests/profile/serializers/test_indicator_data_serializer.py
+++ tests/profile/serializers/test_indicator_data_serializer.py
@@ -50,7 +50,7 @@ def test_profile_indicator_order(geography, profile_indicators):
     pi2.order = 2
     pi2.save()
 
-    output = get_indicator_data(profile, geography)
+    output = get_indicator_data(profile, [geography])
     assert output[0]["profile_indicator_label"] == "PI1"
     assert output[1]["profile_indicator_label"] == "PI2"
 
@@ -60,7 +60,7 @@ def test_profile_indicator_order(geography, profile_indicators):
     pi2.order = 1
     pi2.save()
 
-    output = get_indicator_data(profile, geography)
+    output = get_indicator_data(profile, [geography])
     assert output[0]["profile_indicator_label"] == "PI2"
     assert output[1]["profile_indicator_label"] == "PI1"
 
@@ -68,7 +68,7 @@ def test_profile_indicator_order(geography, profile_indicators):
 @pytest.mark.usefixtures("indicator_data")
 def test_profile_indicator_metadata(geography, profile_indicators, metadata):
     profile = profile_indicators[0].profile
-    output = get_indicator_data(profile, geography)
+    output = get_indicator_data(profile, [geography])
     assert output[0]["metadata_source"] == "A source"
     assert output[0]["metadata_description"] == "A description"
     assert output[0]["metadata_url"] == "http://example.com"
