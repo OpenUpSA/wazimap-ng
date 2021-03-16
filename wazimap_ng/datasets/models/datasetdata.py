2c14d1696a452cab74fe9e5948f89baa2e94bfe6
khadija <khadijamahanga@y7mail.com>
2021-03-16 10:55:48 +0300

    revert unique subindicator

--- wazimap_ng/datasets/models/datasetdata.py
+++ wazimap_ng/datasets/models/datasetdata.py
@@ -19,16 +19,14 @@ class DatasetDataQuerySet(models.QuerySet):
         else:
             return self
 
-    def get_unique_subindicators(self, groups):
+    def get_unique_subindicators(self, group):
 
         # The empty order_by is necessary as Django added any field in order_by to the select clause as well.
         # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.distinct
-        if not isinstance(groups, list):
-            groups = [groups]
 
-        groups = [f"data__{group}" if "data__" not in group else group for group in groups]
+        group = f"data__{group}" if "data__" not in group else group
 
-        return self.order_by().values_list(*groups).distinct()
+        return self.order_by().values_list(group, flat=True).distinct()
 
 
     def grouped_totals_by_geography(self, groups):
