dd1172ca930ae033f473b390c2c41e43a1ba9749
khadija <khadijamahanga@y7mail.com>
2021-03-18 10:20:09 +0300

    rearrange fields

--- wazimap_ng/datasets/admin/forms/indicator_director_admin_form.py
+++ wazimap_ng/datasets/admin/forms/indicator_director_admin_form.py
@@ -4,7 +4,7 @@ from ...models import GeographyHierarchy
 from wazimap_ng.profile.models import Profile
 
 class IndicatorDirectorForm(forms.Form):
-    dataset_file = forms.FileField(required=True)
     profile = forms.ModelChoiceField(queryset=Profile.objects.all(), required=True)
     geography_hierarchy = forms.ModelChoiceField(queryset=GeographyHierarchy.objects.all(), required=True)
+    dataset_file = forms.FileField(required=True)
     indicator_director_file = forms.FileField(required=True)
