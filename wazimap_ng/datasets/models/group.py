c9cdd98d14b88a1b9998ae43a117b87d2fa1afa7
Adi Eyal <adi@openup.org.za>
2021-03-22 10:14:25 +0200

    Added can_aggregate and can_filter to Groups

--- wazimap_ng/datasets/models/group.py
+++ wazimap_ng/datasets/models/group.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from django.db.models.fields import BooleanField
 
 from .dataset import Dataset
 from wazimap_ng.general.models import BaseModel
@@ -8,6 +9,8 @@ class Group(BaseModel):
     name = models.CharField(max_length=100, blank=False, null=False)
     dataset = models.ForeignKey(Dataset, null=True, on_delete=models.CASCADE)
     subindicators = JSONField(blank=True, default=list)
+    can_aggregate = BooleanField(default=True)
+    can_filter = BooleanField(default=True)
 
     def __str__(self):
         return f"{self.dataset.name}|{self.name}"
