dd220181e168a2d21aae89c907353720860632a0
Adi Eyal <adi@openup.org.za>
2021-03-22 10:16:25 +0200

    Added can_aggregate and can_filter to Groups

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from factory.declarations import List
 
 from tests.datasets.factories import (
     DatasetDataFactory,
@@ -6,6 +7,7 @@ from tests.datasets.factories import (
     DatasetFileFactory,
     GeographyFactory,
     GeographyHierarchyFactory,
+    GroupFactory,
     IndicatorDataFactory,
     IndicatorFactory,
     LicenceFactory,
@@ -15,17 +17,17 @@ from tests.profile.factories import (
     IndicatorCategoryFactory,
     IndicatorSubcategoryFactory,
     ProfileFactory,
+    ProfileHighlightFactory,
     ProfileIndicatorFactory,
-    ProfileKeyMetricsFactory,
-    ProfileHighlightFactory
+    ProfileKeyMetricsFactory
 )
 from wazimap_ng.datasets.models import Geography, GeographyHierarchy
-
 
 
 @pytest.fixture
 def licence():
     return LicenceFactory(name="licence name", url="abc url")
+
 
 @pytest.fixture
 def geographies():
@@ -35,9 +37,11 @@ def geographies():
 
     return [root, geo1, geo2]
 
+
 @pytest.fixture
 def geography(geographies):
     return geographies[0]
+
 
 @pytest.fixture
 def geography_hierarchy(geography):
@@ -45,12 +49,14 @@ def geography_hierarchy(geography):
 
     return hierarchy
 
+
 @pytest.fixture
 def child_geographies(geography):
     return [
         geography.add_child(code=f"child{i}_geo", version=geography.version)
         for i in range(2)
     ]
+
 
 @pytest.fixture
 def profile(geography_hierarchy):
@@ -63,10 +69,21 @@ def profile(geography_hierarchy):
 
     return _profile
 
-    
+
 @pytest.fixture
 def dataset(profile):
     return DatasetFactory(profile=profile)
+
+
+@pytest.fixture
+def groups(dataset):
+    return [
+        GroupFactory(dataset=dataset, name="gender", subindicators=[
+                     "male", "female"], can_aggregate=True, can_filter=True),
+        GroupFactory(dataset=dataset, name="language", subindicators=[
+                     "isiXhosa", "isiZulu"], can_aggregate=True, can_filter=True),
+    ]
+
 
 @pytest.fixture
 def indicator(dataset):
@@ -74,24 +91,38 @@ def indicator(dataset):
     groups = ["gender"]
     return IndicatorFactory(dataset=dataset, subindicators=subindicators, groups=groups)
 
+
 @pytest.fixture
 def datasetdata(indicator, geography):
     dataset = indicator.dataset
-    
+
     return [
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "male", "age": "15", "language": "isiXhosa", "count": 1}),
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "male", "age": "15", "language": "isiZulu", "count": 2}),
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "male", "age": "16", "language": "isiXhosa", "count": 3}),
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "male", "age": "16", "language": "isiZulu", "count": 4}),
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "male", "age": "17", "language": "isiXhosa", "count": 5}),
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "male", "age": "17", "language": "isiZulu", "count": 6}),
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "female", "age": "15", "language": "isiXhosa", "count": 7}),
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "female", "age": "15", "language": "isiZulu", "count": 8}),
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "female", "age": "16", "language": "isiXhosa", "count": 9}),
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "female", "age": "16", "language": "isiZulu", "count": 10}),
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "female", "age": "17", "language": "isiXhosa", "count": 11}),
-        DatasetDataFactory(dataset=dataset, geography=geography, data={"gender": "female", "age": "17", "language": "isiZulu", "count": 12}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "male", "age": "15", "language": "isiXhosa", "count": 1}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "male", "age": "15", "language": "isiZulu", "count": 2}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "male", "age": "16", "language": "isiXhosa", "count": 3}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "male", "age": "16", "language": "isiZulu", "count": 4}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "male", "age": "17", "language": "isiXhosa", "count": 5}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "male", "age": "17", "language": "isiZulu", "count": 6}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "female", "age": "15", "language": "isiXhosa", "count": 7}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "female", "age": "15", "language": "isiZulu", "count": 8}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "female", "age": "16", "language": "isiXhosa", "count": 9}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "female", "age": "16", "language": "isiZulu", "count": 10}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "female", "age": "17", "language": "isiXhosa", "count": 11}),
+        DatasetDataFactory(dataset=dataset, geography=geography, data={
+                           "gender": "female", "age": "17", "language": "isiZulu", "count": 12}),
     ]
+
 
 @pytest.fixture
 def metadata(licence):
@@ -123,16 +154,18 @@ def indicatordata(indicator, indicatordata_json, geography):
         IndicatorDataFactory(indicator=indicator, geography=geography, data=indicatordata_json)
     ]
 
+
 @pytest.fixture
 def child_indicatordata(indicator, indicatordata_json, child_geographies):
-    mult_count = lambda js, factor: {"count": js["count"] * factor}
-    merge = lambda d1, d2: {**d1, **d2}
-    dup_data = lambda factor: [merge(js, mult_count(js, factor)) for js in indicatordata_json]
+    def mult_count(js, factor): return {"count": js["count"] * factor}
+    def merge(d1, d2): return {**d1, **d2}
+    def dup_data(factor): return [merge(js, mult_count(js, factor)) for js in indicatordata_json]
 
     return [
         IndicatorDataFactory(indicator=indicator, geography=g, data=dup_data(idx + 1))
         for idx, g in enumerate(child_geographies)
     ]
+
 
 @pytest.fixture
 def profile_indicator(profile, indicatordata):
@@ -144,6 +177,7 @@ def profile_indicator(profile, indicatordata):
 def profile_key_metric(profile, indicatordata):
     indicator = indicatordata[0].indicator
     return ProfileKeyMetricsFactory(profile=profile, variable=indicator, subindicator=1)
+
 
 @pytest.fixture
 def profile_highlight(profile, indicatordata):
