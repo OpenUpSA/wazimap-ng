6b76d30c8aa229313dd386ef6068b276ff9ebf83
Faisal <faysalali534@gmail.com>
2021-04-09 01:39:33 +0500

    removed migrations and used already updated field as last updated

--- wazimap_ng/profile/serializers/profile_indicator_serializer.py
+++ wazimap_ng/profile/serializers/profile_indicator_serializer.py
@@ -5,12 +5,16 @@ from .. import models
 class ProfileIndicatorSerializer(serializers.ModelSerializer):
   subcategory = serializers.SerializerMethodField()
   category = serializers.SerializerMethodField()
+  last_updated = serializers.SerializerMethodField()
 
   def get_category(self, obj):
       return obj.subcategory.category.name
 
   def get_subcategory(self, obj):
       return obj.subcategory.name
+
+  def get_last_updated(self, obj):
+      return obj.updated
 
   class Meta:
       model = models.ProfileIndicator
