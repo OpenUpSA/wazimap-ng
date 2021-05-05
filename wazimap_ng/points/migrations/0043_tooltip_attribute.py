f16b2a694374c9b853671d06dc1242d64ebee11d
Ibaakee Ledum <ledumibaakee@gmail.com>
2021-05-05 09:11:12 +0100

    added data migration for visible_tooltip_attribute

---
+++ wazimap_ng/points/migrations/0043_tooltip_attribute.py
@@ -0,0 +1,26 @@
+from django.db import migrations
+from wazimap_ng.points.models import ProfileCategory
+
+def save_profile_category(apps, schema_editor):
+    query = ProfileCategory.objects.all()
+
+    for data in query:
+        tooltip = data.visible_tooltip_attributes
+        new_tooltip_data = []
+
+        if isinstance(tooltip, dict):
+            new_tooltip_data = tooltip.values()
+
+        data.visible_tooltip_attributes = new_tooltip_data
+        data.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('points', '0042_auto_20210421_0453'),
+    ]
+
+    operations = [
+        migrations.RunPython(save_profile_category),
+    ]
