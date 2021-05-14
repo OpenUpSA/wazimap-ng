44646ea006b0c745ffe662c8a61154386540a7f8
Mila Frerichs <mila.frerichs@gmail.com>
2021-05-14 11:07:55 +0200

    chore: merge migrations and use save ay to get model

--- wazimap_ng/points/migrations/0042_auto_20210421_0453.py
+++ wazimap_ng/points/migrations/0042_auto_20210421_0453.py
@@ -3,6 +3,17 @@
 import django.contrib.postgres.fields.jsonb
 from django.db import migrations
 
+def save_profile_category(apps, schema_editor):
+    ProfileCategory = apps.get_model('points', 'ProfileCategory')
+    for pc in ProfileCategory.objects.all():
+        tooltip = pc.visible_tooltip_attributes
+        new_tooltip_data = []
+
+        if isinstance(tooltip, dict):
+            new_tooltip_data = list(tooltip.values())
+
+        pc.visible_tooltip_attributes = new_tooltip_data
+        pc.save()
 
 class Migration(migrations.Migration):
 
@@ -16,4 +27,5 @@ class Migration(migrations.Migration):
             name='visible_tooltip_attributes',
             field=django.contrib.postgres.fields.jsonb.JSONField(blank=True, default=list, null=True),
         ),
+        migrations.RunPython(save_profile_category, lambda apps, schema_editor: migrations.RunPython.noop),
     ]
