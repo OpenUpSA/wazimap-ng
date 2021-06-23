c0acbb0dda1cd60e13c190f518106767b64a05da
Gaurav Goyal <goyal1092@gmail.com>
2021-06-23 18:22:48 +0530

    Removed defaults from admin description

--- wazimap_ng/datasets/admin/views/metadata_inline_view.py
+++ wazimap_ng/datasets/admin/views/metadata_inline_view.py
@@ -9,9 +9,3 @@ class MetaDataInline(admin.StackedInline):
 
     verbose_name = ""
     verbose_name_plural = "Metadata"
-
-
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == "description":
-            db_field.default = "Representation of data gathered from various sources"
-        return super().formfield_for_dbfield(db_field, **kwargs)
