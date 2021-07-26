b198e2f844d8552f50fe88724ccc3f3543be5eb6
Gaurav Goyal <goyal1092@gmail.com>
2021-07-26 14:26:21 +0530

    Update admin js to use template literals

--- wazimap_ng/general/static/js/variable-filter-widget.js
+++ wazimap_ng/general/static/js/variable-filter-widget.js
@@ -8,8 +8,8 @@
             let permissionType = $(this).val();
             let hiddenPermissionType = permissionType == "public" ? "private": "public";
             let parent = $(this).parents("div");
-            parent.find("#id_indicator").find("[data-type='"+permissionType+"']").removeClass("hidden");
-            parent.find("#id_indicator").find("[data-type='"+hiddenPermissionType+"']").addClass("hidden");
+            parent.find("#id_indicator").find(`[data-type=${permissionType}]`).removeClass("hidden");
+            parent.find("#id_indicator").find(`[data-type=${hiddenPermissionType}]`).addClass("hidden");
             parent.find("#id_indicator option[value='']").attr('selected', true);
         }
     });
