d4e8149fc6d7a7d250e5c8380d674f3009b3882f
Gaurav Goyal <goyal1092@gmail.com>
2021-07-30 17:39:30 +0530

    Added session authentication so admin user is able access api from admin panel

--- wazimap_ng/config/common.py
+++ wazimap_ng/config/common.py
@@ -283,6 +283,7 @@ class Common(QCluster, Configuration):
         ),
         'DEFAULT_AUTHENTICATION_CLASSES': [
             "rest_framework.authentication.TokenAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
         ],
         'DEFAULT_PERMISSION_CLASSES': [
             "wazimap_ng.profile.authentication.ProfilePermissions",
