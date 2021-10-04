d48f45ce6792bce041cb5e1f5e0e35bbd1530b08
esir <esirkings@gmail.com>
2021-10-04 16:16:52 +0300

    Alter indicator category icon from a charfield to an image field

--- wazimap_ng/profile/admin/admins/indicator_category_admin.py
+++ wazimap_ng/profile/admin/admins/indicator_category_admin.py
@@ -13,16 +13,10 @@ from icon_picker_widget.widgets import IconPickerWidget
 class IndicatorCategoryAdminForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['icon'].widget = IconPickerWidget()
 
 @admin.register(models.IndicatorCategory)
 class IndicatorCategoryAdmin(SortableAdminMixin, BaseAdminModel):
     list_display = ("name", "profile", "order")
     list_filter = (filters.ProfileFilter,)
     form = IndicatorCategoryAdminForm
-
-    class Media:
-        css = {
-             'all': ('/static/css/admin-custom.css',)
-        }
 
