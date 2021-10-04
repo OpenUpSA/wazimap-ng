5c24e9f04577fa41d8c7fcbe2b8db40063663065
esir <esirkings@gmail.com>
2021-10-04 16:26:29 +0300

    Format file

--- wazimap_ng/profile/admin/admins/indicator_category_admin.py
+++ wazimap_ng/profile/admin/admins/indicator_category_admin.py
@@ -1,22 +1,20 @@
 from adminsortable2.admin import SortableAdminMixin
-
-from django.contrib.gis import admin
 from django import forms
+from django.contrib.gis import admin
+
+from wazimap_ng.general.admin import filters
+from wazimap_ng.general.admin.admin_base import BaseAdminModel
 
 from ... import models
-from wazimap_ng.general.admin.admin_base import BaseAdminModel
-from wazimap_ng.general.admin import filters
-
-from icon_picker_widget.widgets import IconPickerWidget
 
 
 class IndicatorCategoryAdminForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+
 @admin.register(models.IndicatorCategory)
 class IndicatorCategoryAdmin(SortableAdminMixin, BaseAdminModel):
     list_display = ("name", "profile", "order")
     list_filter = (filters.ProfileFilter,)
     form = IndicatorCategoryAdminForm
-
