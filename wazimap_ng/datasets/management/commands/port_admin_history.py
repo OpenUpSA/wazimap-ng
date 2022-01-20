48ffb55c0df50f66b5521f85c8fcf7f49e96d856
Gaurav <goyal1092@gmail.com>
2022-01-20 15:30:37 +0530

    Added transaction atomic in case of error while running script

--- wazimap_ng/datasets/management/commands/port_admin_history.py
+++ wazimap_ng/datasets/management/commands/port_admin_history.py
@@ -4,26 +4,34 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import FieldError
 from django.contrib.admin.models import LogEntry
 from django.utils import timezone
+from django.db import transaction
 
 
 class Command(BaseCommand):
     help = 'Port django admin log to simple history'
 
+    @transaction.atomic
     def handle(self, *args, **options):
 
         log_entry_objs = LogEntry.objects.all()
         print(f"Started process for {log_entry_objs.count()} log entry objects")
         for log_entry in log_entry_objs:
+
+            # Get instance model
             ct = log_entry.content_type
-            try:
-                instance_model = ct.model_class()
-            except Exception as e:
+            instance_model = ct.model_class()
+
+            # In case we have moved apps from model
+            # we moved around bunch of models on intial stage
+            if not instance_model:
                 continue
             try:
+                # Get instance
                 instance = instance_model.objects.get(id=log_entry.object_id)
             except (instance_model.DoesNotExist, FieldError):
                 continue
 
+            # Get history manager
             try:
                 manager = instance.history
             except AttributeError:
