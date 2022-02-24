bc56b6f193db91b800f68493592eee51aab4b420
JD Bothma <jbothma@gmail.com>
2022-02-24 12:48:48 +0200

    Use os.getenv instead of django-environ

--- wazimap_ng/config/common.py
+++ wazimap_ng/config/common.py
@@ -104,13 +104,13 @@ class Common(QCluster, Configuration):
     WSGI_APPLICATION = "wazimap_ng.wsgi.application"
 
     # Email
-    EMAIL_BACKEND = env.str("EMAIL_BACKEND", 'django.core.mail.backends.smtp.EmailBackend')
-    EMAIL_HOST = env.str("EMAIL_HOST", None)
-    EMAIL_HOST_USER = env.str("EMAIL_HOST_USER", None)
-    EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD", None)
+    EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", 'django.core.mail.backends.smtp.EmailBackend')
+    EMAIL_HOST = os.getenv("EMAIL_HOST", None)
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", None)
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", None)
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL", "webmaster@localhost")
+    DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "webmaster@localhost")
 
     # Postgres
     DATABASES = {
