db650d37448aebdd3a5c1de44a02863ed910e6d5
emreyildirim2038@gmail.com <emreyildirim2038@gmail.com>
2022-03-14 15:40:00 +0300

    working on the ui changes

--- wazimap_ng/general/static/js/profile-indicator-admin.js
+++ wazimap_ng/general/static/js/profile-indicator-admin.js
@@ -3,16 +3,26 @@
         const $profileEl = $(document).find("#id_profile");
         const $subcategoryEl = $(document).find("#id_subcategory");
         const $emptyOptionEl = $("<option></option>");
+        const $permissionOptionEl = $(document).find("#variable-permission-filter");
+
+        filterVariables($profileEl.val(), $permissionOptionEl.find("input:checked").val());
 
         $profileEl.on('change', function (e) {
             // trigger "loadSubcategory" func only when it was manually changed
             if (e.originalEvent !== undefined) {
                 // load subcategories based on selected profile
                 loadSubcategory(e.target.value);
+                filterVariables(e.target.value, $permissionOptionEl.find("input:checked").val());
             }
         });
 
-        function appendOptionEl($outerEl, val, text){
+        $permissionOptionEl.on('change', function (e) {
+            if (e.originalEvent !== undefined) {
+                filterVariables($profileEl.val(), e.target.value);
+            }
+        });
+
+        function appendOptionEl($outerEl, val, text) {
             $outerEl.append($emptyOptionEl.clone().attr("value", val).text(text));
         }
 
@@ -37,6 +47,15 @@
             } else {
                 clearSubcategories();
             }
+        }
+
+        function filterVariables(selectedProfile, permissionType) {
+            $('select#id_indicator option').each(function () {
+                if (this.value !== '') {
+                    //filter out the first option
+                    console.log({'this.value': this})
+                }
+            })
         }
     });
 })(django.jQuery);
