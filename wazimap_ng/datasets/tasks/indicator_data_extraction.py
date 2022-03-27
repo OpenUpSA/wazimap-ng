23f18e320966be37934558f63b2e46923753aa3f
Gaurav <goyal1092@gmail.com>
2022-03-28 02:46:41 +0530

    Fix out of memory issue for indicator data task execution

--- wazimap_ng/datasets/tasks/indicator_data_extraction.py
+++ wazimap_ng/datasets/tasks/indicator_data_extraction.py
@@ -7,27 +7,24 @@ from .. import models
 
 logger = logging.getLogger(__name__)
 
+
 @transaction.atomic
 def indicator_data_extraction(indicator, *args, universe=None, **kwargs):
 
     indicator.indicatordata_set.all().delete()
-
-    data_rows = []
-    datasetdata_qs = models.DatasetData.objects.filter(dataset=indicator.dataset).order_by("geography", "id")
-
+    datasetdata_qs = (
+        models.DatasetData.objects.filter(dataset=indicator.dataset)
+        .order_by("geography", "id")
+        .values("geography_id", "data")
+    )
     if universe is not None:
         datasetdata_qs = datasetdata_qs.filter(**universe)
 
-    grouped_datasetdata = groupby(datasetdata_qs, lambda dd: dd.geography)
+    grouped_datasetdata = groupby(datasetdata_qs, lambda dd: dd["geography_id"])
 
     for g, geography_data in grouped_datasetdata:
-        indicator_data = models.IndicatorData(indicator=indicator, geography=g)
-        indicator_data.data = [
-            dd.data for dd in geography_data
-        ]
-
-        data_rows.append(indicator_data)
-
-    objs = models.IndicatorData.objects.bulk_create(data_rows, 1000)
-
-    return objs
+        models.IndicatorData.objects.create(
+            indicator=indicator,
+            geography_id=g,
+            data=[dd["data"] for dd in geography_data],
+        )
