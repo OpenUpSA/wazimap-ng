e7d38cf355bf8f44ce0c29f65b51ac6dc96ec303
JD Bothma <jbothma@gmail.com>
2022-09-29 17:13:22 +0200

    Run migrations and collectstatic on start

--- scripts/django/web.sh
+++ scripts/django/web.sh
@@ -2,4 +2,6 @@
 
 cd /app
 
+python3 manage.py migrate --no-input
+python3 manage.py collectstatic --no-input
 /usr/local/bin/gunicorn wazimap_ng.wsgi --workers=2 -b 0.0.0.0:${PORT:-8000} --forwarded-allow-ips="*"  --reload  --timeout=300
