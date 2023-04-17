1781e2baa9929fd47750f62258c17da4f3105b44
emreyildirim2038@gmail.com <emreyildirim2038@gmail.com>
2023-04-18 02:09:25 +0300

    adding available filter groups to categories endpoint

--- wazimap_ng/profile/serializers/category_serializer.py
+++ wazimap_ng/profile/serializers/category_serializer.py
@@ -2,12 +2,21 @@ from rest_framework import serializers
 
 from .. import models
 
-class ProfileIndicatorSerializer(serializers.ModelSerializer):
 
-  class Meta:
-    model = models.ProfileIndicator
-    depth = 2
-    fields = ["id", "label",]
+class IndicatorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Indicator
+        fields = ["id", "name", ]
+
+
+class ProfileIndicatorSerializer(serializers.ModelSerializer):
+    groups = IndicatorSerializer(source="indicator")
+
+    class Meta:
+        model = models.ProfileIndicator
+        depth = 2
+        fields = ["id", "label", "indicator", "groups"]
+
 
 class IndicatorSubcategorySerializer(serializers.ModelSerializer):
     indicators = ProfileIndicatorSerializer(source="profileindicator_set", many=True)
@@ -16,6 +25,7 @@ class IndicatorSubcategorySerializer(serializers.ModelSerializer):
         model = models.IndicatorSubcategory
         depth = 2
         fields = ["id", "name", "description", "indicators"]
+
 
 class IndicatorCategorySerializer(serializers.ModelSerializer):
     subcategories = serializers.SerializerMethodField()
